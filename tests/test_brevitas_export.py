de00f7fc3efd8da3edadf09611b47426f4d3749e
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-16 16:25:44 +0100

    [Test] factor out model prep code for LFC tests

--- tests/test_brevitas_export.py
+++ tests/test_brevitas_export.py
@@ -8,67 +8,68 @@ import onnx.numpy_helper as nph
 from models.common import get_act_quant, get_quant_linear, get_quant_type, get_stats_op
 from torch.nn import BatchNorm1d, Dropout, Module, ModuleList
 
+FC_OUT_FEATURES = [1024, 1024, 1024]
+INTERMEDIATE_FC_PER_OUT_CH_SCALING = True
+LAST_FC_PER_OUT_CH_SCALING = False
+IN_DROPOUT = 0.2
+HIDDEN_DROPOUT = 0.2
+
+
+class LFC(Module):
+    def __init__(
+        self,
+        num_classes=10,
+        weight_bit_width=None,
+        act_bit_width=None,
+        in_bit_width=None,
+        in_ch=1,
+        in_features=(28, 28),
+    ):
+        super(LFC, self).__init__()
+
+        weight_quant_type = get_quant_type(weight_bit_width)
+        act_quant_type = get_quant_type(act_bit_width)
+        in_quant_type = get_quant_type(in_bit_width)
+        stats_op = get_stats_op(weight_quant_type)
+
+        self.features = ModuleList()
+        self.features.append(get_act_quant(in_bit_width, in_quant_type))
+        self.features.append(Dropout(p=IN_DROPOUT))
+        in_features = reduce(mul, in_features)
+        for out_features in FC_OUT_FEATURES:
+            self.features.append(
+                get_quant_linear(
+                    in_features=in_features,
+                    out_features=out_features,
+                    per_out_ch_scaling=INTERMEDIATE_FC_PER_OUT_CH_SCALING,
+                    bit_width=weight_bit_width,
+                    quant_type=weight_quant_type,
+                    stats_op=stats_op,
+                )
+            )
+            in_features = out_features
+            self.features.append(BatchNorm1d(num_features=in_features))
+            self.features.append(get_act_quant(act_bit_width, act_quant_type))
+            self.features.append(Dropout(p=HIDDEN_DROPOUT))
+        self.fc = get_quant_linear(
+            in_features=in_features,
+            out_features=num_classes,
+            per_out_ch_scaling=LAST_FC_PER_OUT_CH_SCALING,
+            bit_width=weight_bit_width,
+            quant_type=weight_quant_type,
+            stats_op=stats_op,
+        )
+
+    def forward(self, x):
+        x = x.view(1, 784)
+        x = 2.0 * x - 1.0
+        for mod in self.features:
+            x = mod(x)
+        out = self.fc(x)
+        return out
+
 
 def test_brevitas_to_onnx_export():
-    FC_OUT_FEATURES = [1024, 1024, 1024]
-    INTERMEDIATE_FC_PER_OUT_CH_SCALING = True
-    LAST_FC_PER_OUT_CH_SCALING = False
-    IN_DROPOUT = 0.2
-    HIDDEN_DROPOUT = 0.2
-
-    class LFC(Module):
-        def __init__(
-            self,
-            num_classes=10,
-            weight_bit_width=None,
-            act_bit_width=None,
-            in_bit_width=None,
-            in_ch=1,
-            in_features=(28, 28),
-        ):
-            super(LFC, self).__init__()
-
-            weight_quant_type = get_quant_type(weight_bit_width)
-            act_quant_type = get_quant_type(act_bit_width)
-            in_quant_type = get_quant_type(in_bit_width)
-            stats_op = get_stats_op(weight_quant_type)
-
-            self.features = ModuleList()
-            self.features.append(get_act_quant(in_bit_width, in_quant_type))
-            self.features.append(Dropout(p=IN_DROPOUT))
-            in_features = reduce(mul, in_features)
-            for out_features in FC_OUT_FEATURES:
-                self.features.append(
-                    get_quant_linear(
-                        in_features=in_features,
-                        out_features=out_features,
-                        per_out_ch_scaling=INTERMEDIATE_FC_PER_OUT_CH_SCALING,
-                        bit_width=weight_bit_width,
-                        quant_type=weight_quant_type,
-                        stats_op=stats_op,
-                    )
-                )
-                in_features = out_features
-                self.features.append(BatchNorm1d(num_features=in_features))
-                self.features.append(get_act_quant(act_bit_width, act_quant_type))
-                self.features.append(Dropout(p=HIDDEN_DROPOUT))
-            self.fc = get_quant_linear(
-                in_features=in_features,
-                out_features=num_classes,
-                per_out_ch_scaling=LAST_FC_PER_OUT_CH_SCALING,
-                bit_width=weight_bit_width,
-                quant_type=weight_quant_type,
-                stats_op=stats_op,
-            )
-
-        def forward(self, x):
-            x = x.view(1, 784)
-            x = 2.0 * x - 1.0
-            for mod in self.features:
-                x = mod(x)
-            out = self.fc(x)
-            return out
-
     export_onnx_path = "test_output_lfc.onnx"
     lfc = LFC(weight_bit_width=1, act_bit_width=1, in_bit_width=1)
     bo.export_finn_onnx(lfc, (1, 1, 28, 28), export_onnx_path)
