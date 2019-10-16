a8f91178b3067870050426712966925d7fcbe48b
Yaman Umuroglu <maltanar@gmail.com>
2019-10-16 22:18:50 +0100

    [Test] get rid of objdict in LFC test, fixed in dep now

--- tests/test_brevitas_export.py
+++ tests/test_brevitas_export.py
@@ -142,20 +142,3 @@ def test_brevitas_to_onnx_export_and_exec():
     os.remove(dl_ret)
     shutil.rmtree(mnist_onnx_local_dir)
     os.remove(export_onnx_path)
-
-
-class objdict(dict):
-    def __getattr__(self, name):
-        if name in self:
-            return self[name]
-        else:
-            raise AttributeError("No such attribute: " + name)
-
-    def __setattr__(self, name, value):
-        self[name] = value
-
-    def __delattr__(self, name):
-        if name in self:
-            del self[name]
-        else:
-            raise AttributeError("No such attribute: " + name)
