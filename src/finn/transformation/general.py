413ce943fefe1abb5f78aae11ed97091754f0a62
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-17 15:04:49 +0100

    [Transform] add a transform to set initializer values

--- src/finn/transformation/general.py
+++ src/finn/transformation/general.py
@@ -1,5 +1,7 @@
 import copy
 
+from onnx import numpy_helper as np_helper
+
 
 def give_unique_names(model):
     """Give unique names to each node in the graph using enumeration."""
@@ -10,3 +12,22 @@ def give_unique_names(model):
         n.name = "%s_%d" % (n.op_type, node_count)
         node_count += 1
     return new_model
+
+
+def set_initializer(model, tensor_name, tensor_value):
+    """Set the initializer value for tensor with given name."""
+
+    graph = model.graph
+    # convert tensor_value (numpy array) into TensorProto w/ correct name
+    tensor_init_proto = np_helper.from_array(tensor_value)
+    tensor_init_proto.name = tensor_name
+    # first, remove if an initializer already exists
+    init_names = [x.name for x in graph.initializer]
+    try:
+        init_ind = init_names.index(tensor_name)
+        init_old = graph.initializer[init_ind]
+        graph.initializer.remove(init_old)
+    except ValueError:
+        pass
+    # create and insert new initializer
+    graph.initializer.append(tensor_init_proto)
