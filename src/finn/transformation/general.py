c42c3f5abfcc42534c45635c4b809398c2ebdd15
Yaman Umuroglu <maltanar@gmail.com>
2019-10-21 23:32:54 +0100

    [Transform] rename to give_unique_node_names, change to new iface

--- src/finn/transformation/general.py
+++ src/finn/transformation/general.py
@@ -3,14 +3,15 @@ import copy
 import onnx.numpy_helper as np_helper
 
 
-def give_unique_names(model):
+def give_unique_node_names(model):
     """Give unique names to each node in the graph using enumeration."""
     new_model = copy.deepcopy(model)
     node_count = 0
     for n in new_model.graph.node:
         n.name = "%s_%d" % (n.op_type, node_count)
         node_count += 1
-    return new_model
+    # return model_was_changed = False as single iteration is always enough
+    return (new_model, False)
 
 
 def apply_repeated(model, transform):
