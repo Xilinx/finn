d902ca890c06503c2c4d452718fafa0da38788c5
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-24 14:52:47 +0100

    [Transform] add give_readable_tensor_names

--- src/finn/transformation/general.py
+++ src/finn/transformation/general.py
@@ -6,3 +6,21 @@ def give_unique_node_names(model):
         node_count += 1
     # return model_was_changed = False as single iteration is always enough
     return (model, False)
+
+
+def give_readable_tensor_names(model):
+    """Give more human-readable names to all internal tensors. It's recommended
+    to apply give_unique_node_names prior to this transform."""
+    graph = model.graph
+    for n in graph.node:
+        out_num = 0
+        for o in n.output:
+            model.rename_tensor(o, "%s_out%d" % (n.name, out_num))
+            out_num += 1
+        init_in_num = 0
+        for i in n.input:
+            if model.get_initializer(i) is not None:
+                model.rename_tensor(i, "%s_param%d" % (n.name, init_in_num))
+                init_in_num += 1
+    # return model_was_changed = False as single iteration is always enough
+    return (model, False)
