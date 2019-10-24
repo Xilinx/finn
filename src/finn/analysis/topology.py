4c514ec79dd6371c8b0f790bdac7b47a78d0a11c
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-24 15:58:43 +0100

    [Analysis] add node_inputs_in_expected_order

--- src/finn/analysis/topology.py
+++ src/finn/analysis/topology.py
@@ -27,3 +27,21 @@ def all_tensors_f32(model):
     all_tensors = model.make_empty_exec_context().items()
     non_f32_tensors = filter(lambda x: x[1].dtype != np.float32, all_tensors)
     return {"all_tensors_f32": len(list(non_f32_tensors)) == 0}
+
+
+def node_inputs_in_expected_order(model):
+    """Verifies that the node inputs are ordered in the way that FINN expects
+    them. When a node has a mixture of static (= constant, initialized) inputs
+    and dynamic inputs, the dynamic input should come first, followed by the
+    static one. Only verifiable for a small subset of op_types for now."""
+    op_types = ["MatMul", "Conv", "Add", "Mul"]
+    nodes = filter(lambda x: x.op_type in op_types, model.graph.node)
+    all_OK = True
+    for n in nodes:
+        all_OK = all_OK and len(list(n.input)) == 2
+        # input 0 should be dynamic, no initializer
+        all_OK = all_OK and (model.get_initializer(n.input[0]) is None)
+        # input 1 should be static (unless eltwise add)
+        if n.op_type != "Add":
+            all_OK = all_OK and (model.get_initializer(n.input[1]) is not None)
+    return all_OK
