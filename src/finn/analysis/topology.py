9052ed2d61dccc261f4b969bb1377c57514bb4c9
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-24 11:59:15 +0100

    [Analysis] add is_linear and get_per_tensor_fanouts

---
+++ src/finn/analysis/topology.py
@@ -0,0 +1,18 @@
+def is_linear(model):
+    """Checks whether the given model graph is linear. This is done by looking
+    at the fan-out of each tensor. All tensors have a fan-out <= 1 in a linear
+    graph. Returns {"is_linear", Bool}"""
+    per_tensor_fanouts = get_per_tensor_fanouts(model)
+    # check for tensors that have fanout > 1
+    multi_fanouts = list(filter(lambda x: x[1] > 1, per_tensor_fanouts.items()))
+    return {"is_linear": len(multi_fanouts) == 0}
+
+
+def get_per_tensor_fanouts(model):
+    """Returns a dictionary of (tensor_name, tensor_fanout) for the model."""
+    # make execution context to get a list of tensors
+    per_tensor_fanouts = model.make_empty_exec_context()
+    # replace every tensor with its fanout
+    for tensor_name in per_tensor_fanouts.keys():
+        per_tensor_fanouts[tensor_name] = model.get_tensor_fanout(tensor_name)
+    return per_tensor_fanouts
