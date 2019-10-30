8347a9256a7fc15834027a338a639e83be840c94
Yaman Umuroglu <yamanu@xilinx.com>
2019-10-30 11:11:07 +0000

    [Transform] add move_scalar_mul_past_matmul

--- src/finn/transformation/streamline.py
+++ src/finn/transformation/streamline.py
@@ -100,3 +100,50 @@ def move_add_past_mul(model):
         graph_modified = True
     model = model.transform_single(si.infer_shapes)
     return (model, graph_modified)
+
+
+def move_scalar_mul_past_matmul(model):
+    """Move scalar mul operations past matmul operations. We want to have muls
+    next to each other such that they can be collapsed into a single mul."""
+    graph = model.graph
+    nodes_to_remove = []
+    node_ind = 0
+    graph_modified = False
+    for n in graph.node:
+        node_ind += 1
+        if n.op_type == "Mul":
+            consumer = model.find_consumer(n.output[0])
+            if consumer is not None and consumer.op_type == "MatMul":
+                # have: (x) -> add(,B) -> (x+B) -> mul(,A) -> (xA+BA)
+                # want: (x) -> mul(,A) -> (xA) -> add(,BA) -> (xA+BA)
+                # assume input 0 is from the previous layer, input 1 is the
+                # trained (constant) parameter
+                mul_weight_name = n.input[1]
+                matmul_weight_name = consumer.input[1]
+                A = model.get_initializer(mul_weight_name)
+                W = model.get_initializer(matmul_weight_name)
+                assert A is not None
+                assert W is not None
+                start_name = n.input[0]
+                middle_name = n.output[0]
+                end_name = consumer.output[0]
+                if all(x == 1 for x in A.shape):
+                    # if the mul is scalar, we can simply swap the order of ops
+                    # make and insert new nodes
+                    new_matmul = oh.make_node(
+                        "MatMul", [start_name, matmul_weight_name], [middle_name]
+                    )
+                    new_mul = oh.make_node(
+                        "Mul", [middle_name, mul_weight_name], [end_name]
+                    )
+                    graph.node.insert(node_ind, new_matmul)
+                    graph.node.insert(node_ind + 1, new_mul)
+                    # mark old nodes for removal
+                    nodes_to_remove += [n, consumer]
+                    graph_modified = True
+    # delete marked nodes
+    for n in nodes_to_remove:
+        graph.node.remove(n)
+        graph_modified = True
+    model = model.transform_single(si.infer_shapes)
+    return (model, graph_modified)
