92b0106e0c0c85a5cd53a2fe7f6423e43f557a2e
Yaman Umuroglu <yamanu@xilinx.com>
2019-11-01 16:10:35 +0000

    [Exec] remove print statement from execute_custom_node

--- src/finn/core/execute_custom_node.py
+++ src/finn/core/execute_custom_node.py
@@ -8,17 +8,13 @@ def execute_custom_node(node, context, graph):
     Input/output provided via context."""
 
     if node.op_type == "MultiThreshold":
-
         # save inputs
         v = context[node.input[0]]
         thresholds = context[node.input[1]]
-
         # calculate output
         output = multiThresh.execute(v, thresholds)
-
         # setting context according to output
         context[node.output[0]] = output
-        print(output)
 
     else:
         # exception if op_type is not supported
