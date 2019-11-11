6e19c660eb10c8d2ac73a353aa4c39f6e0b8be91
auphelia <jakobapk@web.de>
2019-11-11 15:12:26 +0000

    [Execution] Added if tree to read out the attribute backend to enable the path for executing of hls lib custom nodes

--- src/finn/core/execute_custom_node.py
+++ src/finn/core/execute_custom_node.py
@@ -1,21 +1,31 @@
 # import onnx.helper as helper
-
+import sys
 import finn.core.multithreshold as multiThresh
+import finn.core.utils as util
 
 
 def execute_custom_node(node, context, graph):
     """Call custom implementation to execute a single custom node.
     Input/output provided via context."""
 
-    if node.op_type == "MultiThreshold":
-        # save inputs
-        v = context[node.input[0]]
-        thresholds = context[node.input[1]]
-        # calculate output
-        output = multiThresh.execute(v, thresholds)
-        # setting context according to output
-        context[node.output[0]] = output
+    if (util.get_by_name(node.attribute, 'backend')) is not None:
+        if node.op_type == "StreamingMaxPool":
+            sys.exit(0)
+        else:
+            # exception if op_type is not supported
+            raise Exception("This hls lib custom node is currently not supported.")
 
+    
     else:
-        # exception if op_type is not supported
-        raise Exception("This custom node is currently not supported.")
+        if node.op_type == "MultiThreshold":
+            # save inputs
+            v = context[node.input[0]]
+            thresholds = context[node.input[1]]
+            # calculate output
+            output = multiThresh.execute(v, thresholds)
+            # setting context according to output
+            context[node.output[0]] = output
+
+        else:
+            # exception if op_type is not supported
+            raise Exception("This custom node is currently not supported.")
