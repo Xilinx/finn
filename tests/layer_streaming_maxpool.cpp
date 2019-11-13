0aa40d0aab7d8462dd0686babcb44ba5568ffa2b
auphelia <jakobapk@web.de>
2019-11-13 16:32:52 +0000

    [Test] Finished example cpp file as help to create a dynamic file using code generation

--- tests/layer_streaming_maxpool.cpp
+++ tests/layer_streaming_maxpool.cpp
@@ -2,25 +2,56 @@
 #include<complex>
 #include<cstdlib>
 #include<map>
+#include <vector>
 #include "bnn-library.h"
+#include "maxpool.h"
 
 #define ImgDim 4
 #define PoolDim 2
 #define NumChannels 2
 
 int main(){
-	std::cout << "TEST" << std::endl;
+	typedef struct{
+		ap_uint<2> last_data;
+		std::vector<ap_uint<2>> data;
+		bool valid;
+		bool invert;
+	} input_interface;
+	input_interface i, k;
 	cnpy::NpyArray arr = cnpy::npy_load("input_0.npy");
 	float* loaded_data = arr.data<float>();
-	std::cout << arr.shape.size() << std::endl;
 	int Nx = arr.shape[0];
 	int Ny = arr.shape[1];
 	int Nz = arr.shape[2];
-	for(int i = 0; i < Nx*Ny*Nz;i++) {
-		std::cout << loaded_data[i] << std::endl;
+	
+	hls::stream<ap_uint<2>> in ("in");
+	hls::stream<ap_uint<2>> out ("out");
+	#pragma HLS DATAFLOW
+	#pragma HLS stream depth=1024 variable=in
+	#pragma HLS stream depth=1024 variable=out
+	for(int i=0;i < Nx*Ny*Nz;i++){
+		in << loaded_data[i];
 	}
-	hls::stream<ap_uint<2>> in;
-	cnpy::npy_save("output.npy",&loaded_data[0],{Nx,Ny,Nz},"w");
+        //while(in.read_nb(i.last_data)){
+        //       i.data.push_back(i.last_data);
+        //}
+	//for(std::vector<ap_uint<2>>::iterator it = i.data.begin(); it!= i.data.end(); ++it){
+        //        std::cout << "Next value: " << *it << std::endl;
+        //}
+	
+	StreamingMaxPool<ImgDim, PoolDim, NumChannels>(in, out);
+	while(out.read_nb(k.last_data)){
+		k.data.push_back(k.last_data);
+	}
+	std::vector<float> output_data;
+	for(std::vector<ap_uint<2>>::iterator it = k.data.begin(); it!= k.data.end(); ++it){
+		ap_uint<2> test = *it;
+		output_data.push_back(test.range(0,0));
+		output_data.push_back(test.range(1,1));
+
+
+	}
+	cnpy::npy_save("output.npy",&output_data[0],{2,2,2},"w");
 
 
 }
--- tests/layer_test.sh
+++ tests/layer_test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-g++ -o layer_streaming_maxpool layer_streaming_maxpool.cpp /workspace/finn/cnpy/cnpy.cpp -I/workspace/finn/cnpy/ -I/workspace/finn/finn-hlslib --std=c++11 -lz
+g++ -o layer_streaming_maxpool layer_streaming_maxpool.cpp /workspace/finn/cnpy/cnpy.cpp -I/workspace/finn/cnpy/ -I/workspace/finn/finn-hlslib -I/workspace/vivado-hlslib --std=c++11 -lz
 
