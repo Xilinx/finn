1c8310570d7dc88ecdc167aabb73c346f431335f
auphelia <jakobapk@web.de>
2019-11-18 15:11:30 +0000

    [Structure] Integrated previous code generation for single node into new structue (as functions in streaminmaxpool.py) and deleted old file

--- src/finn/custom_op/fpgadataflow/__init__.py
+++ src/finn/custom_op/fpgadataflow/__init__.py
@@ -5,8 +5,67 @@ from finn.custom_op import CustomOp
 class HLSCustomOp(CustomOp):
     def __init__(self):
         super().__init__()
+        # template for single node execution
+        self.docompute_template= """
+        #include "cnpy.h"
+        #include <vector>
+        #include "bnn-library.h"
 
+        // includes for network parameters
+        $GLOBALS$
+
+        // defines for network parameters
+        $DEFINES$
+
+        int main(){
+
+        $STREAMDECLARATIONS$
+    
+        $READNPYDATA$
+
+        $DOCOMPUTE$
+    
+        $DATAOUTSTREAM$
+
+        $SAVEASCNPY$
+    
+        }
+
+        """
+    
+    @abstractmethod
+    def get_attributes(self, node):
+        pass
 
     @abstractmethod
     def code_generation(self, node):
         pass
+    
+    @abstractmethod
+    def global_includes(self, node, code_gen_dict):
+        pass
+
+    @abstractmethod
+    def defines(self, node, code_gen_dict):
+        pass
+
+    @abstractmethod
+    def read_npy_data(self, node, code_gen_dict):
+        pass
+
+    @abstractmethod
+    def strm_decl(self, node, code_gen_dict):
+        pass
+
+    @abstractmethod
+    def docompute(self, node, code_gen_dict):
+        pass
+    
+    @abstractmethod
+    def dataoutstrm(self, node, code_gen_dict):
+        pass
+
+    @abstractmethod
+    def save_as_npy(self, node, code_gen_dict):
+        pass
+
--- src/finn/custom_op/fpgadataflow/streamingmaxpool.py
+++ src/finn/custom_op/fpgadataflow/streamingmaxpool.py
@@ -6,7 +6,6 @@ import os
 from finn.core.datatype import DataType
 from finn.core.utils import get_by_name
 from finn.custom_op.fpgadataflow import HLSCustomOp
-import finn.backend.fpgadataflow.code_gen_for_single_node_execution as code_gen
 
 
 class StreamingMaxPool(HLSCustomOp):
@@ -23,7 +22,7 @@ class StreamingMaxPool(HLSCustomOp):
             np.save("input_{}.npy".format(in_ind), context[inputs])
             temp_files.append("input_{}.npy".format(in_ind))
             in_ind += 1
-        code_gen.execute(node, context, graph)
+        self.code_generation(node)
         output = np.load("output.npy")
         for i in range(output.shape[0]):
             print(np.transpose(output[i]))
@@ -32,5 +31,62 @@ class StreamingMaxPool(HLSCustomOp):
         #    os.remove(temp_file)
         sys.exit(0)
 
+    def get_attributes(self, node):
+        self.ImgDim = get_by_name(node.attribute, 'ImgDim').i
+        self.PoolDim = get_by_name(node.attribute, 'PoolDim').i
+        self.NumChannels = get_by_name(node.attribute, 'NumChannels').i
+
     def code_generation(self, node):
+        code_gen_dict={}
+        self.get_attributes(node)
+        self.global_includes(node, code_gen_dict)
+        self.defines(node, code_gen_dict)
+        self.read_npy_data(node, code_gen_dict)
+        self.strm_decl(node, code_gen_dict)
+        self.docompute(node, code_gen_dict)
+        
+        print(code_gen_dict)
+
+    def global_includes(self, node, code_gen_dict):
+        code_gen_dict["$GLOBALS$"]=['#include "maxpool.h"']
+
+    def defines(self, node, code_gen_dict):
+         code_gen_dict["$DEFINES$"]=['#define ImgDim {}\n #define PoolDim {}\n #define NumChannels {}'.format(self.ImgDim, self.PoolDim, self.NumChannels)]
+
+    def read_npy_data(self, node, code_gen_dict):
+        code_gen_dict["$READNPDATA$"]=[]
+        input_ind = 0
+        input_file_names = []
+        for inputs in node.input:
+            input_file_names.append("input_{}.npy".format(input_ind))
+            input_ind += 1
+
+        input_ind = 0
+        for input_file in input_file_names:
+            code_gen_dict["$READNPDATA$"].append('cnpy::NpyArray arr = cnpy::npy_load("{}");\n float* loaded_data{} = arr.data<float>();'.format(input_file, input_ind))
+            code_gen_dict["$READNPDATA$"].append('int num_values = 1; \n for(int i = 0; i < arr.shape.size(); i++){\n num_values *= arr.shape[i]; \n }')
+            code_gen_dict["$READNPDATA$"].append('ap_uint<{}> dat;'.format(self.NumChannels))
+            code_gen_dict["$READNPDATA$"].append('for(int i=0; i < num_values; i+=2){')
+            for channel in range(self.NumChannels):
+                code_gen_dict["$READNPDATA$"].append('dat.range({},{}) = loaded_data{}[i+{}];'.format(channel, channel, input_ind, channel))
+                code_gen_dict["$READNPDATA$"].append('in{} << loaded_data{}[dat];'.format(input_ind, input_ind))
+                code_gen_dict["$READNPDATA$"].append('}')
+            input_ind+=1
+
+    def strm_decl(self, node, code_gen_dict):
+        code_gen_dict["$STREAMDECLARATIONS$"]=[]
+        input_ind = 0
+        for inputs in node.input:
+            code_gen_dict["$STREAMDECLARATIONS$"].append('hls::stream<ap_uint<{}>> in{} ("in{}");'.format(self.NumChannels, input_ind, input_ind))
+            input_ind += 1
+        code_gen_dict["$STREAMDECLARATIONS$"].append('hls::stream<ap_uint<{}>> out ("out");'.format(self.NumChannels))
+
+    def docompute(self, node, code_gen_dict):
+        code_gen_dict["$DOCOMPUTE$"] = ['{}<ImgDim, PoolDim, NumChannels>(in0, out);'.format(node.op_type)]
+
+    
+    def dataoutstrm(self, node, code_gen_dict):
+        pass
+
+    def save_as_npy(self, node, code_gen_dict):
         pass
