c4598aec1248b603644c89b5d825b540a0eecb3f
auphelia <jakobapk@web.de>
2019-11-20 16:22:35 +0000

    [Code gen] Commit of current state of work, test cpp file to check syntax for implementing weights and threshold activation

--- src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
+++ src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
@@ -18,7 +18,7 @@ class StreamingFCLayer_Batch(HLSCustomOp):
         self.resDataType = get_by_name(node.attribute, "resDataType").s.decode("utf-8")
 
     def global_includes(self, node):
-        self.code_gen_dict["$GLOBALS$"] = [""]
+        self.code_gen_dict["$GLOBALS$"] = ['// no additional includes necessary']
 
     def defines(self, node):
         numReps = 2
@@ -30,16 +30,104 @@ class StreamingFCLayer_Batch(HLSCustomOp):
         ]
 
     def read_npy_data(self, node):
-        pass
+        self.code_gen_dict["$READNPYDATA$"] = []
+        input_ind = 0
+        input_file_names = []
+        for inputs in node.input:
+            input_file_names.append("input_{}.npy".format(input_ind))
+            input_ind += 1
+
+        input_ind = 0
+        for input_file in input_file_names:
+            self.code_gen_dict["$READNPYDATA$"].append(
+                """cnpy::NpyArray arr{} = cnpy::npy_load("{}");\n
+                float* loaded_data{} = arr{}.data<float>();""".format(
+                    input_ind, input_file, input_ind, input_ind
+                )
+            )
+            if input_ind == 0:
+                self.code_gen_dict["$READNPYDATA$"].append(
+                    """int num_values{} = 1; \n
+                    for(int i = 0; i < arr{}.shape.size(); i++){{\n
+                    num_values{} *= arr{}.shape[i]; \n }}""".format(
+                        input_ind, input_ind, input_ind, input_ind
+                    )
+                )
+                self.code_gen_dict["$READNPYDATA$"].append(
+                    "ap_uint<{}> dat{};".format(self.SIMD, input_ind)
+                )
+                self.code_gen_dict["$READNPYDATA$"].append(
+                    "for(int i=0; i < num_values{}/{}; i++){{".format(input_ind, self.SIMD)
+                )
+                for line in range(self.SIMD):
+                    self.code_gen_dict["$READNPYDATA$"].append(
+                        "dat{}.range({},{}) = loaded_data{}[i+((num_values{}/{})*{})];".format(
+                            input_ind, line, line, input_ind, input_ind, self.SIMD, line
+                        )
+                    )
+                self.code_gen_dict["$READNPYDATA$"].append("in{} << dat{};".format(input_ind, input_ind))
+                self.code_gen_dict["$READNPYDATA$"].append("}")
+            input_ind += 1
+ 
 
     def strm_decl(self, node):
-        pass
+        self.code_gen_dict["$STREAMDECLARATIONS$"] = []
+        input_ind = 0
+        for inputs in node.input:
+            self.code_gen_dict["$STREAMDECLARATIONS$"].append(
+                'hls::stream<ap_uint<{}>> in{} ("in{}");'.format(
+                    self.SIMD, input_ind, input_ind
+                )
+            )
+            input_ind += 1
+        self.code_gen_dict["$STREAMDECLARATIONS$"].append(
+            'hls::stream<ap_uint<{}>> out ("out");'.format(self.PE)
+        )
+ 
 
     def docompute(self, node):
-        pass
+        self.code_gen_dict["$DOCOMPUTE$"] = [
+            "{}<MW, MH, SIMD, PE, {}>(in0, loaded_data1, loaded_data2, out, numReps, {});".format(node.op_type, self.resDataType, self.resType)
+        ]
 
     def dataoutstrm(self, node):
-        pass
+        self.code_gen_dict["$DATAOUTSTREAM$"] = [
+            "ap_uint<{}> out_data;\n std::vector<ap_uint<{}>> out_data_vector;".format(
+                self.PE, self.PE
+            )
+        ]
+        self.code_gen_dict["$DATAOUTSTREAM$"].append("while(out.read_nb(out_data)){")
+        self.code_gen_dict["$DATAOUTSTREAM$"].append(
+            "out_data_vector.push_back(out_data);\n}"
+        )
+        self.code_gen_dict["$DATAOUTSTREAM$"].append(
+            "std::vector<float> output_data_vector;"
+        )
+        self.code_gen_dict["$DATAOUTSTREAM$"].append(
+            """for(std::vector<ap_uint<{}>>::iterator it = out_data_vector.begin();
+            it != out_data_vector.end(); ++it){{""".format(
+                self.PE
+            )
+        )
+        self.code_gen_dict["$DATAOUTSTREAM$"].append(
+            "ap_uint<{}> output_data = *it;".format(self.PE)
+        )
+        for element in range(self.PE):
+            self.code_gen_dict["$DATAOUTSTREAM$"].append(
+                "output_data_vector.push_back(output_data.range({},{}));".format(
+                    element, element
+                )
+            )
+        self.code_gen_dict["$DATAOUTSTREAM$"].append("}")
+
 
     def save_as_npy(self, node):
-        pass
+        numReps = 2
+        self.code_gen_dict["$SAVEASCNPY$"] = [
+            """cnpy::npy_save("output.npy",&output_data_vector[0],
+            {{1,{},{}}},"w");""".format(
+                self.PE,
+                self.PE,
+            )
+        ]
+
