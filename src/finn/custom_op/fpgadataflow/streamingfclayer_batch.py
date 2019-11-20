c8a2cda13ee643ed444a38b125dadbe9a9d6ac4c
auphelia <jakobapk@web.de>
2019-11-20 11:15:06 +0000

    [Exec and code gen] Added function to get all attributes of an streaming fclayer batch

--- src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
+++ src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
@@ -8,3 +8,32 @@ class StreamingFCLayer_Batch(HLSCustomOp):
 
     def infer_node_datatype(self, node, model):
         pass
+
+    def get_attributes(self, node):
+        self.resType = get_by_name(node.attribute, "resType").s.decode("utf-8")
+        self.MW = get_by_name(node.attribute, "MW").i
+        self.MH = get_by_name(node.attribute, "MH").i
+        self.SIMD = get_by_name(node.attribute, "SIMD").i
+        self.PE = get_by_name(node.attribute, "PE").i
+        self.resDataType = get_by_name(node.attribute, "resDataType").s.decode("utf-8")
+
+    def global_includes(self, node):
+        pass
+
+    def defines(self, node):
+        pass
+
+    def read_npy_data(self, node):
+        pass
+    
+    def strm_decl(self, node):
+        pass
+
+    def docompute(self, node):
+        pass
+    
+    def dataoutstrm(self, node):
+        pass
+
+    def save_as_npy(self, node):
+        pass
