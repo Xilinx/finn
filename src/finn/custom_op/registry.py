2ceb009736498fd735346c95441785a9ba7adaa9
Yaman Umuroglu <maltanar@gmail.com>
2019-12-03 00:09:53 +0000

    [CustomOp] remove non-batch StreamingMaxPool for now

--- src/finn/custom_op/registry.py
+++ src/finn/custom_op/registry.py
@@ -1,8 +1,7 @@
 # make sure new CustomOp subclasses are imported here so that they get
 # registered and plug in correctly into the infrastructure
-from finn.custom_op.fpgadataflow.streamingmaxpool import StreamingMaxPool
-from finn.custom_op.fpgadataflow.streamingmaxpool_batch import StreamingMaxPool_Batch
 from finn.custom_op.fpgadataflow.streamingfclayer_batch import StreamingFCLayer_Batch
+from finn.custom_op.fpgadataflow.streamingmaxpool_batch import StreamingMaxPool_Batch
 from finn.custom_op.multithreshold import MultiThreshold
 from finn.custom_op.xnorpopcount import XnorPopcountMatMul
 
@@ -11,6 +10,5 @@ custom_op = {}
 
 custom_op["MultiThreshold"] = MultiThreshold
 custom_op["XnorPopcountMatMul"] = XnorPopcountMatMul
-custom_op["StreamingMaxPool"] = StreamingMaxPool
 custom_op["StreamingMaxPool_Batch"] = StreamingMaxPool_Batch
 custom_op["StreamingFCLayer_Batch"] = StreamingFCLayer_Batch
