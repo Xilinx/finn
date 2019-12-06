5f51bf98c1053ac8076b1819a8d03d2a271595c4
Yaman Umuroglu <maltanar@gmail.com>
2019-12-06 01:39:29 +0000

    [Transform] add clipping to RoundThresholds and rename

--- src/finn/transformation/streamline/round_thresholds.py
+++ src/finn/transformation/streamline/round_thresholds.py
@@ -3,9 +3,10 @@ import numpy as np
 from finn.transformation import Transformation
 
 
-class RoundThresholds(Transformation):
+class RoundAndClipThresholds(Transformation):
     """For MultiThreshold nodes operating on integer inputs, round up
-    thresholds values to the nearest integer."""
+    thresholds values to the nearest integer. Additionally, if the input
+    is unsigned, sets negative thresholds to zero."""
 
     def apply(self, model):
         graph = model.graph
@@ -17,6 +18,13 @@ class RoundThresholds(Transformation):
                 Tnew = np.ceil(T)
                 if idtype.is_integer() and (T != Tnew).any():
                     # round up the thresholds to nearest integer
+                    model.set_initializer(n.input[1], Tnew)
+                    # use same datatype as inputs for thresholds
+                    model.set_tensor_datatype(n.input[1], idtype)
+                    graph_modified = True
+                if idtype.is_integer() and not idtype.signed() and (Tnew < 0).any():
+                    # clip any negative thresholds
+                    Tnew = np.clip(Tnew, 0, None)
                     model.set_initializer(n.input[1], Tnew)
                     # use same datatype as inputs for thresholds
                     model.set_tensor_datatype(n.input[1], idtype)
