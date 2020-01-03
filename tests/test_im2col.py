488f2fc1ed3312162bff9ccfa7f7093462c86af0
auphelia <jakobapk@web.de>
2020-01-03 12:15:09 +0000

    [Test] Added test function for infer data shapes with im2col node embedded between two standard onnx nodes

--- tests/test_im2col.py
+++ tests/test_im2col.py
@@ -6,6 +6,7 @@ from finn.core.datatype import DataType
 from finn.core.modelwrapper import ModelWrapper
 from finn.core.utils import gen_finn_dt_tensor
 from finn.transformation.infer_datatypes import InferDataTypes
+from finn.transformation.infer_shapes import InferShapes
 
 
 def check_two_dict_for_equality(dict1, dict2):
@@ -39,7 +40,13 @@ def test_im2col():
     )
 
     Im2Col_node = helper.make_node(
-        "Im2Col", ["inp"], ["outp"], domain="finn", stride=stride, kernel_size=k,
+        "Im2Col",
+        ["inp"],
+        ["outp"],
+        domain="finn",
+        stride=stride,
+        kernel_size=k,
+        input_shape="(1,{},{},{})".format(ifm_ch, ifm_dim, ifm_dim),
     )
 
     graph = helper.make_graph(
@@ -50,6 +57,10 @@ def test_im2col():
     model = ModelWrapper(model)
 
     model.set_tensor_datatype("inp", idt)
+
+    # test shape inference
+    model.transform(InferShapes())
+    assert model.get_tensor_shape("outp") == [1, out_pix, k * k * ifm_ch]
 
     # test datatype inference
     assert model.get_tensor_datatype("outp") is DataType.FLOAT32
@@ -83,3 +94,59 @@ def test_im2col():
     print(x)
     print("Output:")
     print(y_produced)
+
+
+def test_im2col_infer_shapes():
+    idt = DataType.BIPOLAR
+    k = 2
+    stride = 1
+    ifm_ch = 1
+    ifm_dim = 4
+    ofm_dim = int(((ifm_dim - k) / stride) + 1)
+    out_pix = ofm_dim * ofm_dim
+
+    # set up onnx model
+    inp = helper.make_tensor_value_info(
+        "inp", TensorProto.FLOAT, [1, ifm_ch, ifm_dim, ifm_dim]
+    )
+    outp = helper.make_tensor_value_info(
+        "outp", TensorProto.FLOAT, [1, out_pix, k * k * ifm_ch]
+    )
+
+    abs_node = helper.make_node("Abs", inputs=["inp"], outputs=["abs"],)
+
+    Im2Col_node = helper.make_node(
+        "Im2Col",
+        ["abs"],
+        ["im2col"],
+        domain="finn",
+        stride=stride,
+        kernel_size=k,
+        input_shape="(1,{},{},{})".format(ifm_ch, ifm_dim, ifm_dim),
+    )
+
+    abs1_node = helper.make_node("Abs", inputs=["im2col"], outputs=["outp"],)
+
+    graph = helper.make_graph(
+        nodes=[abs_node, Im2Col_node, abs1_node],
+        name="shape_graph",
+        inputs=[inp],
+        outputs=[outp],
+        value_info=[
+            helper.make_tensor_value_info(
+                "abs", TensorProto.FLOAT, [1, ifm_ch, ifm_dim, ifm_dim]
+            ),
+            helper.make_tensor_value_info(
+                "im2col", TensorProto.FLOAT, [1, out_pix, k * k * ifm_ch]
+            ),
+        ],
+    )
+
+    model = helper.make_model(graph, producer_name="shape-model")
+    model = ModelWrapper(model)
+
+    model.set_tensor_datatype("inp", idt)
+
+    # test shape inference
+    model.transform(InferShapes())
+    assert model.get_tensor_shape("im2col") == [1, out_pix, k * k * ifm_ch]
