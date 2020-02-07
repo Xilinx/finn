e4bb7c2df8deb4b0735f4867b746eb975260dba6
Yaman Umuroglu <maltanar@gmail.com>
2020-02-07 18:30:03 +0100

    [Refactor] move onnx-importing util to own file

--- src/finn/util/basic.py
+++ src/finn/util/basic.py
@@ -5,7 +5,6 @@ import subprocess
 import tempfile
 
 import numpy as np
-import onnx
 
 from finn.core.datatype import DataType
 
@@ -36,15 +35,6 @@ def make_build_dir(prefix=""):
         correctly. Please ensure you have launched the Docker contaier correctly.
         """
         )
-
-
-def valueinfo_to_tensor(vi):
-    """Creates an all-zeroes numpy tensor from a ValueInfoProto."""
-
-    dims = [x.dim_value for x in vi.type.tensor_type.shape.dim]
-    return np.zeros(
-        dims, dtype=onnx.mapping.TENSOR_TYPE_TO_NP_TYPE[vi.type.tensor_type.elem_type]
-    )
 
 
 def get_by_name(container, name, name_field="name"):
