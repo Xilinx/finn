07b3b2dbab2ba3cb497788d807a8d7542236d1c8
Yaman Umuroglu <maltanar@gmail.com>
2020-02-07 18:17:41 +0100

    [Driver] flesh out and fill driver template

--- src/finn/transformation/fpgadataflow/make_pynq_driver.py
+++ src/finn/transformation/fpgadataflow/make_pynq_driver.py
@@ -2,7 +2,8 @@ import os
 import shutil
 
 from finn.transformation import Transformation
-from finn.util.basic import get_finn_root, make_build_dir
+from finn.util.basic import gen_finn_dt_tensor, get_finn_root, make_build_dir
+from finn.util.data_packing import finnpy_to_packed_bytearray
 
 from . import templates
 
@@ -30,13 +31,40 @@ class MakePYNQDriver(Transformation):
         pynq_driver_dir = make_build_dir(prefix="pynq_driver_")
         model.set_metadata_prop("pynq_driver_dir", pynq_driver_dir)
 
-        # generate the driver
+        # extract input-output shapes from the graph
+        # TODO convert this to an analysis pass
+        i_tensor_name = model.graph.input[0].name
+        o_tensor_name = model.graph.output[0].name
+        i_tensor_shape = model.get_tensor_shape(i_tensor_name)
+        o_tensor_shape = model.get_tensor_shape(o_tensor_name)
+        i_tensor_dt = model.get_tensor_datatype(i_tensor_name)
+        o_tensor_dt = model.get_tensor_datatype(o_tensor_name)
+        # generate dummy i/o tensors and their packed versions
+        i_tensor_dummy = gen_finn_dt_tensor(i_tensor_dt, i_tensor_shape)
+        o_tensor_dummy = gen_finn_dt_tensor(o_tensor_dt, o_tensor_shape)
+        i_tensor_dummy_packed = finnpy_to_packed_bytearray(i_tensor_dummy, i_tensor_dt)
+        o_tensor_dummy_packed = finnpy_to_packed_bytearray(o_tensor_dummy, o_tensor_dt)
+        i_tensor_shape_packed = i_tensor_dummy_packed.shape
+        o_tensor_shape_packed = o_tensor_dummy_packed.shape
+
+        # fill in the driver template
         driver_py = pynq_driver_dir + "/driver.py"
+        driver = templates.pynq_driver_template
+        driver = driver.replace("$INPUT_FINN_DATATYPE$", str(i_tensor_dt))
+        driver = driver.replace("$INPUT_SHAPE_UNPACKED$", str(i_tensor_shape))
+        driver = driver.replace("$INPUT_SHAPE_PACKED$", str(i_tensor_shape_packed))
+        driver = driver.replace("$OUTPUT_FINN_DATATYPE$", str(o_tensor_dt))
+        driver = driver.replace("$OUTPUT_SHAPE_PACKED$", str(o_tensor_shape_packed))
+        driver = driver.replace("$OUTPUT_SHAPE_UNPACKED$", str(o_tensor_shape))
+
         with open(driver_py, "w") as f:
-            f.write(templates.pynq_driver_template)
+            f.write(driver)
         # copy all the dependencies into the driver folder
         shutil.copytree(
             get_finn_root() + "/src/finn/util", pynq_driver_dir + "/finn/util"
+        )
+        shutil.copytree(
+            get_finn_root() + "/src/finn/core", pynq_driver_dir + "/finn/core"
         )
 
         return (model, False)
