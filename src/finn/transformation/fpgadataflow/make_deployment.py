5979b1120237741ebad72bf006916f62347c0a34
auphelia <jakobapk@web.de>
2020-02-12 13:24:53 +0000

    [Transformation] Added functionality to make_deployment trafo to collect all necessary files in one folder

--- src/finn/transformation/fpgadataflow/make_deployment.py
+++ src/finn/transformation/fpgadataflow/make_deployment.py
@@ -1,4 +1,9 @@
+import os
+from distutils.dir_util import copy_tree
+from shutil import copy
+
 from finn.transformation import Transformation
+from finn.util.basic import make_build_dir
 
 
 class DeployToPYNQ(Transformation):
@@ -16,9 +21,30 @@ class DeployToPYNQ(Transformation):
         self.target_dir = target_dir
 
     def apply(self, model):
+        # set metadata properties accordingly to user input specifications
         model.set_metadata_prop("pynq_ip", self.ip)
         model.set_metadata_prop("pynq_username", self.username)
         model.set_metadata_prop("pynq_password", self.password)
         model.set_metadata_prop("pynq_target_dir", self.target_dir)
+
+        # create directory for deployment files
+        deployment_dir = make_build_dir(prefix="pynq_deployment_")
+        model.set_metadata_prop("pynq_deployment_dir", deployment_dir)
+
+        # get and copy necessary files
+        # .bit and .hwh file
+        vivado_pynq_proj = model.get_metadata_prop("vivado_pynq_proj")
+        for file in os.listdir(vivado_pynq_proj):
+            if file.endswith(".bit"):
+                bitfile = os.path.join(vivado_pynq_proj, file)
+            elif file.endswith(".hwh"):
+                hwhfile = os.path.join(vivado_pynq_proj, file)
+        copy(bitfile, deployment_dir)
+        copy(hwhfile, deployment_dir)
+
+        # driver.py and python libraries
+        pynq_driver_dir = model.get_metadata_prop("pynq_driver_dir")
+        copy_tree(pynq_driver_dir, deployment_dir)
+        model.set_metadata_prop("pynq_deploy_dir", deployment_dir)
 
         return (model, False)
