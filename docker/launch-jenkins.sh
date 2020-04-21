d7da11c6d22f5a656d19dace9c7765d0ae2c8c08
Yaman Umuroglu <maltanar@gmail.com>
2020-04-21 01:47:50 +0100

    [Jenkins] allow overriding certain config from envvars

--- docker/launch-jenkins.sh
+++ docker/launch-jenkins.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+JENKINS_USER ?= jenkins
+JENKINS_PORT ?= 8080
+JENKINS_HOME ?= $(pwd)/jenkins_home
+
+mkdir -p $JENKINS_HOME
+
 docker build -t finn_jenkins -f Dockerfile.jenkins .
-docker run -p 8080:8080 -p 50000:50000 -v /var/run/docker.sock:/var/run/docker.sock -v jenkins_home:/var/jenkins_home finn_jenkins
+docker run -u $JENKINS_USER -p $JENKINS_PORT:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $JENKINS_HOME:/var/jenkins_home finn_jenkins
