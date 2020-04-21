e786a577048b050839072ce2963eeaf6ab2d2a17
Yaman Umuroglu <maltanar@gmail.com>
2020-04-21 17:24:34 +0100

    [Jenkins] add comments in launch-jenkins, run entire test suite

--- docker/launch-jenkins.sh
+++ docker/launch-jenkins.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-: ${JUPYTER_PORT=8888}
-: ${JENKINS_USER=jenkins}
-: ${JENKINS_PORT=8080}
-: ${JENKINS_HOME=$(pwd)/jenkins_home}
+# defaults, can be overriden by environment variables
+# user to run Jenkins as -- see NOTE below regarding Docker access permissions
+JENKINS_USER ?= jenkins
+# port for Jenkins on host machine
+JENKINS_PORT ?= 8080
+# make Jenkins config persistent by mounting into this folder
+JENKINS_HOME ?= $(pwd)/jenkins_home
 
 mkdir -p $JENKINS_HOME
 
+# build a Jenkins Docker image that also has the Docker CLI installed
 docker build -t finn_jenkins -f Dockerfile.jenkins .
+
+# launch Docker container mounted to local Docker socket
+# NOTE: we allow customizing the user (e.g. as root) to work around permission
+# issues, may not al
 docker run -u $JENKINS_USER -p $JENKINS_PORT:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $JENKINS_HOME:/var/jenkins_home finn_jenkins
