9d5ac20739c27990dbabbfbdcdaba95f116f7138
auphelia <jakobapk@web.de>
2020-06-09 14:48:23 +0100

    [Test] Add transformation and execution function to mobilnet test

--- tests/brevitas/test_brevitas_mobilenet.py
+++ tests/brevitas/test_brevitas_mobilenet.py
@@ -5,6 +5,11 @@ import brevitas.onnx as bo
 import torch
 from finn.util.basic import make_build_dir
 from finn.util.test import get_test_model_trained
+from finn.core.modelwrapper import ModelWrapper
+from finn.transformation.infer_shapes import InferShapes
+from finn.transformation.infer_datatypes import InferDataTypes
+from finn.transformation.general import GiveUniqueNodeNames
+import finn.core.onnx_exec as oxe
 
 
 def test_brevitas_mobilenet():
@@ -29,9 +34,18 @@ def test_brevitas_mobilenet():
     assert input_tensor.shape == (1, 3, 224, 224)
     # do forward pass in PyTorch/Brevitas
     expected = mobilenet.forward(input_tensor).detach().numpy()
-    expected = expected.flatten()
-    winner_inds_top5 = np.argsort(expected)[-5:]
-    winner_ind = winner_inds_top5[-1]
-    winner_prob = expected[winner_ind]
-    assert winner_prob != 0
+    # expected = expected.flatten()
+    # winner_inds_top5 = np.argsort(expected)[-5:]
+    # winner_ind = winner_inds_top5[-1]
+    # winner_prob = expected[winner_ind]
+    # assert winner_prob != 0
     bo.export_finn_onnx(mobilenet, (1, 3, 224, 224), finn_onnx)
+    model = ModelWrapper(finn_onnx)
+    model = model.transform(InferShapes())
+    model = model.transform(InferDataTypes())
+    model = model.transform(GiveUniqueNodeNames())
+    model.save("quant_mobilenet_v1_4b.onnx")
+    idict = {model.graph.input[0].name: img}
+    odict = oxe.execute_onnx(model, idict, True)
+    produced = odict[model.graph.output[0].name]
+    assert (produced == expected).all()
