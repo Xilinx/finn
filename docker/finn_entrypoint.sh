25a06fdb8ad5897d67e6a1bffc11532e9658cd60
auphelia <jakobapk@web.de>
2020-06-10 11:09:50 +0100

    [Docker] Update brevitas commit version

--- docker/finn_entrypoint.sh
+++ docker/finn_entrypoint.sh
@@ -13,7 +13,7 @@ gecho () {
 
 # checkout the correct dependency repo commits
 # the repos themselves are cloned in the Dockerfile
-BREVITAS_COMMIT=093de7d138c6715dbcaf82a9e1d530069327ad98
+BREVITAS_COMMIT=8ef72897dbda4fcf605443ff0e86ffd8844ec8b2
 CNPY_COMMIT=4e8810b1a8637695171ed346ce68f6984e585ef4
 HLSLIB_COMMIT=13e9b0772a27a3a1efc40c878d8e78ed09efb716
 PYVERILATOR_COMMIT=1d89cb0d4e0c97469cc6352c611f876ec13edfa6
