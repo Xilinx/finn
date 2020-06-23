f19e3121e7a54b429b6c43816c85ced8e8916116
auphelia <jakobapk@web.de>
2020-06-23 11:53:16 +0100

    [Test] Update test to check functional verification after MoveFlatten trafo

--- tests/transformation/test_move_flatten.py
+++ tests/transformation/test_move_flatten.py
@@ -4,10 +4,15 @@ from onnx import TensorProto, helper
 from finn.core.modelwrapper import ModelWrapper
 from finn.core.datatype import DataType
 from finn.util.basic import gen_finn_dt_tensor
+from finn.transformation.infer_shapes import InferShapes
+from finn.transformation.infer_datatypes import InferDataTypes
+from finn.transformation.general import GiveUniqueNodeNames, GiveReadableTensorNames
+from finn.transformation.streamline.reorder import MoveFlatten
+import finn.core.onnx_exec as oxe
 
 
 def test_move_flatten():
-    inp = helper.make_tensor_value_info("inp", TensorProto.FLOAT, [1, 1024, 1, 1])
+    inp = helper.make_tensor_value_info("inp", TensorProto.FLOAT, [1, 1, 1, 1024])
     a0 = helper.make_tensor_value_info("a0", TensorProto.FLOAT, [])
     shape = helper.make_tensor_value_info("shape", TensorProto.INT64, [2])
     a1 = helper.make_tensor_value_info("a1", TensorProto.FLOAT, [1024, 1000])
@@ -42,15 +47,25 @@ def test_move_flatten():
     model = ModelWrapper(model)
 
     # initialize values
-    a0_values = np.random.uniform(low=0.1, high=0.99, size=(1))
+    a0_values = np.random.uniform(low=0.1, high=0.99, size=(1)).astype(np.float32)
     model.set_initializer("a0", a0_values)
-    shape_values = np.asarray([1, -1]).astype(np.float32)
+    shape_values = np.asarray([1, -1]).astype(np.int64)
     model.set_initializer("shape", shape_values)
     a1_values = gen_finn_dt_tensor(DataType.TERNARY, [1024, 1000])
     model.set_initializer("a1", a1_values)
     a2_values = 1.0 / a0_values
     model.set_initializer("a2", a2_values)
-    a3_values = np.random.uniform(low=-1, high=1, size=(1000))
+    a3_values = np.random.uniform(low=-1, high=1, size=(1000)).astype(np.float32)
     model.set_initializer("a3", a3_values)
-    model.set_initializer("k", np.asarray([5]))
-    model.save("test.onnx")
+    model.set_initializer("k", np.asarray([5]).astype(np.int64))
+
+    model = model.transform(InferShapes())
+    model = model.transform(InferDataTypes())
+    model = model.transform(GiveUniqueNodeNames())
+    model = model.transform(GiveReadableTensorNames())
+
+    # compare execution before and after transformation
+    inp_values = gen_finn_dt_tensor(DataType.INT2, [1, 1, 1, 1024])
+    idict = {"inp": inp_values}
+    model_transformed = model.transform(MoveFlatten())
+    assert oxe.compare_execution(model, model_transformed, idict)
