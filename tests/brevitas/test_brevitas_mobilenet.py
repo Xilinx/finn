528103defd530a95e2de6c2b1f6ee6fdb332d449
auphelia <jakobapk@web.de>
2020-07-06 16:22:25 +0100

    [Test] Set input finn dtype and fix bug with saving onnx checkpoints

--- tests/brevitas/test_brevitas_mobilenet.py
+++ tests/brevitas/test_brevitas_mobilenet.py
@@ -7,6 +7,7 @@ from finn.util.basic import make_build_dir
 from finn.util.pytorch import NormalizePreProc
 from finn.util.test import get_test_model_trained, resize_smaller_side, crop_center
 from finn.core.modelwrapper import ModelWrapper
+from finn.core.datatype import DataType
 from finn.transformation.infer_shapes import InferShapes
 from finn.transformation.infer_data_layouts import InferDataLayouts
 from finn.transformation.fold_constants import FoldConstants
@@ -51,19 +52,21 @@ def test_brevitas_mobilenet():
 
     # export preprocess
     export_onnx_path = make_build_dir("test_brevitas_mobilenet-v1_")
-    preproc_onnx = export_onnx_path + "quant_mobilenet_v1_4b_preproc.onnx"
+    preproc_onnx = export_onnx_path + "/quant_mobilenet_v1_4b_preproc.onnx"
     mean = [0.485, 0.456, 0.406]
     std = 0.226
     ch = 3
     preproc = NormalizePreProc(mean, std, ch)
     bo.export_finn_onnx(preproc, (1, 3, 224, 224), preproc_onnx)
     preproc_model = ModelWrapper(preproc_onnx)
+    # set input finn datatype to UINT8
+    preproc_model.set_tensor_datatype(preproc_model.graph.input[0].name, DataType.UINT8)
     preproc_model = preproc_model.transform(InferShapes())
     preproc_model = preproc_model.transform(GiveUniqueNodeNames())
     preproc_model = preproc_model.transform(GiveUniqueParameterTensors())
     preproc_model = preproc_model.transform(GiveReadableTensorNames())
 
-    finn_onnx = export_onnx_path + "quant_mobilenet_v1_4b.onnx"
+    finn_onnx = export_onnx_path + "/quant_mobilenet_v1_4b_exported.onnx"
     mobilenet = get_test_model_trained("mobilenet", 4, 4)
     bo.export_finn_onnx(mobilenet, (1, 3, 224, 224), finn_onnx)
 
@@ -89,9 +92,9 @@ def test_brevitas_mobilenet():
     model = model.transform(GiveUniqueNodeNames())
     model = model.transform(GiveUniqueParameterTensors())
     model = model.transform(GiveReadableTensorNames())
-    model.save(export_onnx_path + "quant_mobilenet_v1_4b_wo_preproc.onnx")
+    model.save(export_onnx_path + "/quant_mobilenet_v1_4b_wo_preproc.onnx")
     model = model.transform(MergeONNXModels(preproc_model))
-    model.save(export_onnx_path + "quant_mobilenet_v1_4b.onnx")
+    model.save(export_onnx_path + "/quant_mobilenet_v1_4b.onnx")
     idict = {model.graph.input[0].name: img_np}
     odict = oxe.execute_onnx(model, idict, True)
     produced = odict[model.graph.output[0].name]
@@ -116,7 +119,7 @@ def test_brevitas_mobilenet():
     model = model.transform(GiveUniqueNodeNames())
     model = model.transform(GiveReadableTensorNames())
     model = model.transform(InferDataTypes())
-    model.save(export_onnx_path + "quant_mobilenet_v1_4b_streamlined.onnx")
+    model.save(export_onnx_path + "/quant_mobilenet_v1_4b_streamlined.onnx")
     odict_streamline = oxe.execute_onnx(model, idict, True)
     produced_streamline = odict_streamline[model.graph.output[0].name]
     produced_streamline_prob = odict_streamline["TopK_0_out0"] * a0
