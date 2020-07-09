b85ee03df6b70f747ba6ad0811752221a17ccb10
auphelia <jakobapk@web.de>
2020-07-09 17:09:04 +0100

    [Test] Save golden output for end2end mobilenet

--- tests/end2end/test_end2end_mobilenet_v1.py
+++ tests/end2end/test_end2end_mobilenet_v1.py
@@ -26,13 +26,20 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+from PIL import Image
 import os
 import numpy as np
 import brevitas.onnx as bo
+import torch
 
 from finn.custom_op.registry import getCustomOp
 from finn.util.pytorch import NormalizePreProc
-from finn.util.test import get_test_model_trained, load_test_checkpoint_or_skip
+from finn.util.test import (
+    get_test_model_trained,
+    load_test_checkpoint_or_skip,
+    resize_smaller_side,
+    crop_center,
+)
 
 from finn.core.modelwrapper import ModelWrapper
 from finn.core.datatype import DataType
@@ -65,7 +72,8 @@ from finn.transformation.fpgadataflow.create_dataflow_partition import (
 build_dir = "/tmp/" + os.environ["FINN_INST_NAME"]
 
 
-def test_end2end_mobilenet_preprocess_export():
+def test_end2end_mobilenet_export():
+    # export preprocessing
     preproc_onnx = build_dir + "/end2end_mobilenet_preproc.onnx"
     mean = [0.485, 0.456, 0.406]
     std = 0.226
@@ -81,13 +89,39 @@ def test_end2end_mobilenet_preprocess_export():
     preproc_model = preproc_model.transform(GiveReadableTensorNames())
     preproc_model.save(build_dir + "/end2end_mobilenet_preproc.onnx")
 
-
-def test_end2end_mobilenet_export():
+    # export mobilenet
     finn_onnx = build_dir + "/end2end_mobilenet_export.onnx"
     mobilenet = get_test_model_trained("mobilenet", 4, 4)
     bo.export_finn_onnx(mobilenet, (1, 3, 224, 224), finn_onnx)
     model = ModelWrapper(finn_onnx)
     model.save(build_dir + "/end2end_mobilenet_export.onnx")
+
+    # calculate golden output with pytorch/brevitas and save as .npy
+    # get single image as input and prepare image
+    img = Image.open("/workspace/finn/tests/brevitas/king_charles.jpg")
+    # resize smallest side of the image to 256 pixels and resize larger side
+    # with same ratio
+    img = resize_smaller_side(256, img)
+    # crop central 224*224 window
+    img = crop_center(224, img)
+    # save image as numpy array and as torch tensor to enable testing in
+    # brevitas/pytorch and finn and transpose from (H, W, C) to (C, H, W)
+    img_np = np.asarray(img).copy().astype(np.float32).transpose(2, 0, 1)
+    img_np = img_np.reshape(1, 3, 224, 224)
+    np.save(build_dir + "/end2end_mobilenet_input.npy", img_np)
+    img_torch = torch.from_numpy(img_np).float()
+    # do forward pass in PyTorch/Brevitas
+    input_tensor = preproc.forward(img_torch)
+    golden = mobilenet.forward(input_tensor).detach().numpy()
+    golden_topk = golden.flatten()
+    golden_top5 = np.argsort(golden_topk)[-5:]
+    golden_top5 = np.flip(golden_top5)
+    golden_top5_prob = []
+    for index in golden_top5:
+        golden_top5_prob.append(golden_topk[index])
+    # save golden output values
+    np.save(build_dir + "/end2end_mobilenet_golden_top5.npy", golden_top5)
+    np.save(build_dir + "/end2end_mobilenet_golden_top5_prob.npy", golden_top5_prob)
 
 
 def test_end2end_mobilenet_tidy_and_merge_with_preproc():
