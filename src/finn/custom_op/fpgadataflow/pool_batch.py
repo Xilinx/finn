4f92b8c56877f2b1f4edc91f92466438fb860022
auphelia <jakobapk@web.de>
2020-07-21 11:00:33 +0100

    [CustomOp] Add get_exp_cycles for pool_batch and fmpadding

--- src/finn/custom_op/fpgadataflow/pool_batch.py
+++ src/finn/custom_op/fpgadataflow/pool_batch.py
@@ -136,6 +136,10 @@ class Pool_Batch(HLSCustomOp):
         folded_oshape = self.get_folded_output_shape()
         return np.prod(folded_oshape[1:-1])
 
+    def get_exp_cycles(self):
+        # Channels/PE * batch size * odim * odim
+        return np.prod(self.get_folded_output_shape()[:-1])
+
     def get_instream_width(self):
         dt_bits = self.get_input_datatype().bitwidth()
         pe = self.get_nodeattr("PE")
