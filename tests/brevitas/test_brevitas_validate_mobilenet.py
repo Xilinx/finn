38b8982612c0c702e49a1a332930a27835546e96
Yaman Umuroglu <maltanar@gmail.com>
2020-08-26 00:24:08 +0200

    [Test] use new utils in MobileNet-v1 tests

--- tests/brevitas/test_brevitas_validate_mobilenet.py
+++ tests/brevitas/test_brevitas_validate_mobilenet.py
@@ -1,12 +1,11 @@
 import os
 import csv
-from PIL import Image
 import numpy as np
 import brevitas.onnx as bo
 import torch
 from finn.util.basic import make_build_dir
 from finn.util.pytorch import NormalizePreProc, BrevitasDebugHook
-from finn.util.test import get_test_model_trained, resize_smaller_side, crop_center
+from finn.util.test import get_test_model_trained
 from finn.core.modelwrapper import ModelWrapper
 from finn.transformation.infer_shapes import InferShapes
 from finn.transformation.infer_data_layouts import InferDataLayouts
@@ -27,21 +26,38 @@ import pytest
 
 n_images = 10
 debug_mode = False
+# normalization (preprocessing) settings for MobileNet-v1 w4a4
+mean = [0.485, 0.456, 0.406]
+std = 0.226
+ch = 3
+
+
+def test_brevitas_mobilenet_preproc():
+    if "IMAGENET_VAL_PATH" not in os.environ.keys():
+        pytest.skip("Can't do validation without IMAGENET_VAL_PATH")
+    preproc = NormalizePreProc(mean, std, ch)
+    mobilenet = get_test_model_trained("mobilenet", 4, 4)
+
+    ((top1_ok, top1_nok), (top5_ok, top5_nok)) = imagenet_util.measure_topk(
+        n_images,
+        fxn_pre=lambda x: preproc.forward(torch.from_numpy(x).float()),
+        fxn_exec=lambda x: mobilenet.forward(x),
+        fxn_post=lambda x: x.detach().numpy(),
+    )
+    assert (top1_ok, top1_nok) == (7, 3)
+    assert (top5_ok, top5_nok) == (8, 2)
 
 
 @pytest.mark.slow
 # marked as XFAIL until Brevitas export issues are resolved:
 # https://github.com/Xilinx/brevitas/issues/173
 @pytest.mark.xfail
-def test_brevitas_validate_mobilenet():
+def test_brevitas_compare_exported_mobilenet():
     if "IMAGENET_VAL_PATH" not in os.environ.keys():
         pytest.skip("Can't do validation without IMAGENET_VAL_PATH")
     export_onnx_path = make_build_dir("test_brevitas_mobilenet-v1_")
     # export preprocessing
     preproc_onnx = export_onnx_path + "/quant_mobilenet_v1_4b_preproc.onnx"
-    mean = [0.485, 0.456, 0.406]
-    std = 0.226
-    ch = 3
     preproc = NormalizePreProc(mean, std, ch)
     bo.export_finn_onnx(preproc, (1, 3, 224, 224), preproc_onnx)
     preproc_model = ModelWrapper(preproc_onnx)
@@ -76,7 +92,9 @@ def test_brevitas_validate_mobilenet():
     model = model.transform(MergeONNXModels(preproc_model))
     model.save(export_onnx_path + "/quant_mobilenet_v1_4b.onnx")
 
-    with open("mobilenet_validation.csv", "w", newline="") as csvfile:
+    with open(
+        export_onnx_path + "/mobilenet_validation.csv", "w", newline=""
+    ) as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(
             [
@@ -94,17 +112,7 @@ def test_brevitas_validate_mobilenet():
         all_inds_ok = True
         all_probs_ok = True
         for (img_path, target_id) in workload:
-            # get single image as input and prepare image
-            img = Image.open(img_path)
-            # resize smallest side of the image to 256 pixels and resize larger side
-            # with same ratio
-            img = resize_smaller_side(256, img)
-            # crop central 224*224 window
-            img = crop_center(224, img)
-            # save image as numpy array and as torch tensor to enable testing in
-            # brevitas/pytorch and finn and transpose from (H, W, C) to (C, H, W)
-            img_np = np.asarray(img).copy().astype(np.float32).transpose(2, 0, 1)
-            img_np = img_np.reshape(1, 3, 224, 224)
+            img_np = imagenet_util.load_resize_crop(img_path)
             img_torch = torch.from_numpy(img_np).float()
             # do forward pass in PyTorch/Brevitas
             input_tensor = preproc.forward(img_torch)
