f79b701f9690200108897842d7cf1cc2cfe1978f
Yaman Umuroglu <maltanar@gmail.com>
2020-09-01 16:31:52 +0200

    [Deps] update Brevitas to fix dependencies pkg version

--- docker/finn_entrypoint.sh
+++ docker/finn_entrypoint.sh
@@ -12,7 +12,7 @@ gecho () {
 
 # checkout the correct dependency repo commits
 # the repos themselves are cloned in the Dockerfile
-BREVITAS_COMMIT=302ec0490f1ddc956f1df6d0ccd2228f0d50bb65
+BREVITAS_COMMIT=85e28ac2e6570e91216d042212a7d1a28ec6e394
 CNPY_COMMIT=4e8810b1a8637695171ed346ce68f6984e585ef4
 HLSLIB_COMMIT=cfafe11a93b79ab1af7529d68f08886913a6466e
 PYVERILATOR_COMMIT=c97a5ba41bbc7c419d6f25c74cdf3bdc3393174f
