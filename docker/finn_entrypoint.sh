70c8ac62ba8ad91878453757c69526776b0112f9
Yaman Umuroglu <maltanar@gmail.com>
2020-09-10 11:39:27 +0200

    [Deps] remove PYNQ-HelloWorld

--- docker/finn_entrypoint.sh
+++ docker/finn_entrypoint.sh
@@ -12,11 +12,10 @@ gecho () {
 
 # checkout the correct dependency repo commits
 # the repos themselves are cloned in the Dockerfile
-BREVITAS_COMMIT=85e28ac2e6570e91216d042212a7d1a28ec6e394
+BREVITAS_COMMIT=98017609725a6e425e1cdeffb13f175ce815939c
 CNPY_COMMIT=4e8810b1a8637695171ed346ce68f6984e585ef4
 HLSLIB_COMMIT=cfafe11a93b79ab1af7529d68f08886913a6466e
 PYVERILATOR_COMMIT=c97a5ba41bbc7c419d6f25c74cdf3bdc3393174f
-PYNQSHELL_COMMIT=bf281fc3a44eca29efbcbefd63f1196d82c7c255
 OMX_COMMIT=1bae737669901e762f581af73348332b5c4b2ada
 
 gecho "Setting up known-good commit versions for FINN dependencies"
@@ -37,10 +36,6 @@ git -C /workspace/finn-hlslib checkout $HLSLIB_COMMIT --quiet
 gecho "PyVerilator @ $PYVERILATOR_COMMIT"
 git -C /workspace/pyverilator pull --quiet
 git -C /workspace/pyverilator checkout $PYVERILATOR_COMMIT --quiet
-# PYNQ-HelloWorld
-gecho "PYNQ shell @ $PYNQSHELL_COMMIT"
-git -C /workspace/PYNQ-HelloWorld pull --quiet
-git -C /workspace/PYNQ-HelloWorld checkout $PYNQSHELL_COMMIT --quiet
 # oh-my-xilinx
 gecho "oh-my-xilinx @ $OMX_COMMIT"
 git -C /workspace/oh-my-xilinx pull --quiet
