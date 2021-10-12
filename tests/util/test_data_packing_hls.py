bb3197ed21a406182f59d331fbc1dab655a0383c
Yaman Umuroglu <yamanu@xilinx.com>
2021-10-12 16:26:22 +0200

    [Test] add float32 case to test_npy2apintstream

--- tests/util/test_data_packing_hls.py
+++ tests/util/test_data_packing_hls.py
@@ -40,7 +40,13 @@ from finn.util.data_packing import numpy_to_hls_code
 
 @pytest.mark.parametrize(
     "dtype",
-    [DataType["BINARY"], DataType["INT2"], DataType["INT32"], DataType["FIXED<9,6>"]],
+    [
+        DataType["BINARY"],
+        DataType["INT2"],
+        DataType["INT32"],
+        DataType["FIXED<9,6>"],
+        DataType["FLOAT32"],
+    ],
 )
 @pytest.mark.parametrize("test_shape", [(1, 2, 4), (1, 1, 64), (2, 64)])
 @pytest.mark.vivado
