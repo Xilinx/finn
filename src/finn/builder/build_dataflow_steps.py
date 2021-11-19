d6ca2e2e67abcc79f399f9c826e2278c49b52db2
Yaman Umuroglu <yamanu@xilinx.com>
2021-11-20 00:50:31 +0100

    [Build] better resolution for unspec'd Vitis platform for Alveo

--- src/finn/builder/build_dataflow_steps.py
+++ src/finn/builder/build_dataflow_steps.py
@@ -644,7 +644,7 @@ def step_synthesize_bitfile(model: ModelWrapper, cfg: DataflowBuildConfig):
                 VitisBuild(
                     cfg._resolve_fpga_part(),
                     cfg.synth_clk_period_ns,
-                    cfg.vitis_platform,
+                    cfg._resolve_vitis_platform(),
                     strategy=cfg._resolve_vitis_opt_strategy(),
                     enable_debug=cfg.enable_hw_debug,
                     floorplan_file=cfg.vitis_floorplan_file,
