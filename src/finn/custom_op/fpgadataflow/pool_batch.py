27fa5582aeb4538484b7002e93a6f36370d3f863
Yaman Umuroglu <yamanu@xilinx.com>
2021-12-15 21:31:49 +0100

    [Pool] add activations.hpp to pool codegen includes

--- src/finn/custom_op/fpgadataflow/pool_batch.py
+++ src/finn/custom_op/fpgadataflow/pool_batch.py
@@ -200,6 +200,7 @@ class Pool_Batch(HLSCustomOp):
         return info_messages
 
     def global_includes(self):
+        self.code_gen_dict["$GLOBALS$"] = ['#include "activations.hpp"']
         self.code_gen_dict["$GLOBALS$"] = ['#include "maxpool.h"']
         self.code_gen_dict["$GLOBALS$"] += ['#include "pool.hpp"']
 
