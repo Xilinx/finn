3af233b72fc2056bd355bd0265981bc08d004bbf
atchelet <alon.tchelet@gmail.com>
2022-02-04 17:44:24 +0100

    setting up qyolo_finn and import files

--- notebooks/MScThesis/qYOLO/module.py
+++ notebooks/MScThesis/qYOLO/module.py
@@ -2,17 +2,25 @@
 import numpy as np
 import torch
 from kmeans_pytorch import kmeans
-from torch.nn import Module, Sequential, BatchNorm2d, MSELoss
-from brevitas.nn import QuantIdentity, QuantConv2d, QuantReLU, QuantMaxPool2d, QuantSigmoid
+from torch.nn import Module, Sequential, BatchNorm2d
+from brevitas.nn import (
+    QuantIdentity,
+    QuantConv2d,
+    QuantReLU,
+    QuantMaxPool2d,
+    QuantSigmoid,
+)
 from brevitas.inject.defaults import *
 from brevitas.core.restrict_val import RestrictValueType
+
 # qYOLO imports
 from qYOLO.cfg import *
 
 
 class QTinyYOLOv2(Module):
-
-    def __init__(self, n_anchors, weight_bit_width=8, act_bit_width=8, quant_tensor=True):
+    def __init__(
+        self, n_anchors, weight_bit_width=8, act_bit_width=8, quant_tensor=True
+    ):
         super(QTinyYOLOv2, self).__init__()
         self.weight_bit_width = int(np.clip(weight_bit_width, 1, 8))
         self.act_bit_width = int(np.clip(act_bit_width, 1, 8))
@@ -24,73 +32,137 @@ class QTinyYOLOv2(Module):
             max_val=1.0 - 2.0 ** (-7),
             signed=True,
             restrict_scaling_type=RestrictValueType.POWER_OF_TWO,
-            return_quant_tensor=quant_tensor
+            return_quant_tensor=quant_tensor,
         )
         self.conv1 = Sequential(
-            QuantConv2d(3, 16, 3, 1, (2, 2), bias=False,
-                        weight_bit_width=8, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                3,
+                16,
+                3,
+                1,
+                (2, 2),
+                bias=False,
+                weight_bit_width=8,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(16),
             QuantReLU(bit_width=8, return_quant_tensor=quant_tensor),
-            QuantMaxPool2d(2, 2, (1, 1), return_quant_tensor=quant_tensor)
+            QuantMaxPool2d(2, 2, (1, 1), return_quant_tensor=quant_tensor),
         )
         self.conv2 = Sequential(
-            QuantConv2d(16, 32, 3, 1, (2, 1), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                16,
+                32,
+                3,
+                1,
+                (2, 1),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(32),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor),
-            QuantMaxPool2d(2, 2, (0, 1), return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
+            QuantMaxPool2d(2, 2, (0, 1), return_quant_tensor=quant_tensor),
         )
         self.conv3 = Sequential(
-            QuantConv2d(32, 64, 3, 1, (1, 1), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                32,
+                64,
+                3,
+                1,
+                (1, 1),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(64),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor),
-            QuantMaxPool2d(2, 2, (0, 1), return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
+            QuantMaxPool2d(2, 2, (0, 1), return_quant_tensor=quant_tensor),
         )
         self.conv4 = Sequential(
-            QuantConv2d(64, 128, 3, 1, (2, 2), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                64,
+                128,
+                3,
+                1,
+                (2, 2),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(128),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor),
-            QuantMaxPool2d(2, 2, (0, 0), return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
+            QuantMaxPool2d(2, 2, (0, 0), return_quant_tensor=quant_tensor),
         )
         self.conv5 = Sequential(
-            QuantConv2d(128, 256, 3, 1, (1, 2), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                128,
+                256,
+                3,
+                1,
+                (1, 2),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(256),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor),
-            QuantMaxPool2d(2, 2, (0, 0), return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
+            QuantMaxPool2d(2, 2, (0, 0), return_quant_tensor=quant_tensor),
         )
         self.conv6 = Sequential(
-            QuantConv2d(256, 512, 3, 1, (2, 2), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                256,
+                512,
+                3,
+                1,
+                (2, 2),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(512),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor),
-            QuantMaxPool2d(2, 2, (0, 0), return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
+            QuantMaxPool2d(2, 2, (0, 0), return_quant_tensor=quant_tensor),
         )
         self.conv7 = Sequential(
-            QuantConv2d(512, 512, 3, 1, (2, 2), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                512,
+                512,
+                3,
+                1,
+                (2, 2),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(512),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
         )
         self.conv8 = Sequential(
-            QuantConv2d(512, 512, 3, 1, (1, 2), bias=False,
-                        weight_bit_width=self.weight_bit_width, return_quant_tensor=quant_tensor),
+            QuantConv2d(
+                512,
+                512,
+                3,
+                1,
+                (1, 2),
+                bias=False,
+                weight_bit_width=self.weight_bit_width,
+                return_quant_tensor=quant_tensor,
+            ),
             BatchNorm2d(512),
-            QuantReLU(bit_width=self.act_bit_width,
-                      return_quant_tensor=quant_tensor)
+            QuantReLU(bit_width=self.act_bit_width, return_quant_tensor=quant_tensor),
         )
-        self.conv9 = QuantConv2d(512, self.n_anchors*O_SIZE, 1, 1, 0, bias=False, weight_bit_width=8, return_quant_tensor=quant_tensor
-                                 )
-        self.sig = QuantSigmoid(bit_width=8, return_quant_tensor=quant_tensor
-                                )
+        self.conv9 = QuantConv2d(
+            512,
+            self.n_anchors * O_SIZE,
+            1,
+            1,
+            0,
+            bias=False,
+            weight_bit_width=8,
+            return_quant_tensor=quant_tensor,
+        )
+        self.sig = QuantSigmoid(bit_width=8, return_quant_tensor=quant_tensor)
 
     def forward(self, x):
         x = self.input(x)
@@ -120,16 +192,25 @@ def getAnchors(dataset, n_anchors, device):
             datapoints = data
     # k-means clustering
     kmean_idx, anchors = kmeans(
-        X=datapoints, num_clusters=n_anchors, distance='euclidean', device=device)
-
+        X=datapoints, num_clusters=n_anchors, distance="euclidean", device=device
+    )
+    # print anchors
+    print(f"Anchors for k={n_anchors}:")
+    [print(anchor) for anchor in anchors]
     return anchors
 
 
 def YOLOout(output, anchors):
-    gx = ((torch.arange(GRID_SIZE[1]).repeat_interleave(
-        GRID_SIZE[0]*anchors.size(0))) / GRID_SIZE[1]).view(np.prod(GRID_SIZE), anchors.size(0))
-    gy = ((torch.arange(GRID_SIZE[0]).repeat(GRID_SIZE[1])).repeat_interleave(
-        anchors.size(0)) / GRID_SIZE[0]).view(np.prod(GRID_SIZE), anchors.size(0))
+    gx = (
+        (torch.arange(GRID_SIZE[1]).repeat_interleave(GRID_SIZE[0] * anchors.size(0)))
+        / GRID_SIZE[1]
+    ).view(np.prod(GRID_SIZE), anchors.size(0))
+    gy = (
+        (torch.arange(GRID_SIZE[0]).repeat(GRID_SIZE[1])).repeat_interleave(
+            anchors.size(0)
+        )
+        / GRID_SIZE[0]
+    ).view(np.prod(GRID_SIZE), anchors.size(0))
     output[..., 0] = (torch.sigmoid(output[..., 0]) / GRID_SIZE[0]) + gx
     output[..., 1] = (torch.sigmoid(output[..., 1]) / GRID_SIZE[1]) + gy
     output[..., 2] = torch.exp(output[..., 2]) * anchors[:, 0]
--- notebooks/MScThesis/qYOLO/train.py
+++ notebooks/MScThesis/qYOLO/train.py
@@ -4,32 +4,42 @@ import torch.utils.tensorboard
 from torchvision import transforms
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm, trange
+
 # import qYOLO
 from qYOLO.module import *
 from qYOLO.dataset import *
 from qYOLO.util import *
 
 
-def train(img_dir, lbl_dir, weight_bit_width=1, act_bit_width=3, n_anchors=5, n_epochs=100, batch_size=32):
+def train(
+    img_dir,
+    lbl_dir,
+    weight_bit_width=1,
+    act_bit_width=3,
+    n_anchors=5,
+    n_epochs=100,
+    batch_size=32,
+):
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Trainig on: {device}")
 
     # logger
     logger = torch.utils.tensorboard.SummaryWriter()
 
     # dataset
-    transformers = transforms.Compose(
-        [ToTensor(), Normalize()])
+    transformers = transforms.Compose([ToTensor(), Normalize()])
     dataset = YOLO_dataset(img_dir, lbl_dir, transformers)
     data_len = len(dataset)
-    train_len = int(data_len*0.8)
-    test_len = data_len-train_len
+    train_len = int(data_len * 0.8)
+    test_len = data_len - train_len
     train_set, test_set = random_split(dataset, [train_len, test_len])
-    train_loader = DataLoader(train_set, batch_size=batch_size,
-                              shuffle=True, num_workers=4)
-    test_loader = DataLoader(test_set, batch_size=batch_size,
-                             shuffle=True, num_workers=4)
+    train_loader = DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=4
+    )
+    test_loader = DataLoader(
+        test_set, batch_size=batch_size, shuffle=True, num_workers=4
+    )
 
     # get anchors
     anchors = getAnchors(train_set, n_anchors, device)
@@ -45,7 +55,12 @@ def train(img_dir, lbl_dir, weight_bit_width=1, act_bit_width=3, n_anchors=5, n_
         # train + train loss
         train_loss = 0.0
         test_loss = 0.0
-        for i, data in tqdm(enumerate(train_loader, 0), total=int(np.ceil(train_len/batch_size)), desc="train loss", unit="batch"):
+        for i, data in tqdm(
+            enumerate(train_loader, 0),
+            total=int(np.ceil(train_len / batch_size)),
+            desc="train loss",
+            unit="batch",
+        ):
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data[0].to(device), data[1].to(device)
             # zero the parameter gradients
@@ -58,16 +73,19 @@ def train(img_dir, lbl_dir, weight_bit_width=1, act_bit_width=3, n_anchors=5, n_
             train_loss += loss.item()
         # test loss
         with torch.no_grad():
-            for i, data in tqdm(enumerate(test_loader, 0), total=int(np.ceil(test_len/batch_size)), desc="test loss", unit="batch"):
-                test_images, test_labels = data[0].to(
-                    device), data[1].to(device)
+            for i, data in tqdm(
+                enumerate(test_loader, 0),
+                total=int(np.ceil(test_len / batch_size)),
+                desc="test loss",
+                unit="batch",
+            ):
+                test_images, test_labels = data[0].to(device), data[1].to(device)
                 test_outputs = net(test_images)
-                t_loss = loss_func(
-                    test_outputs.value.float(), test_labels.float())
+                t_loss = loss_func(test_outputs.value.float(), test_labels.float())
                 test_loss += t_loss.item()
         # log loss statistics
-        logger.add_scalar('Loss/train', train_loss/train_len, epoch)
-        logger.add_scalar('Loss/test', test_loss/test_len, epoch)
+        logger.add_scalar("Loss/train", train_loss / train_len, epoch)
+        logger.add_scalar("Loss/test", test_loss / test_len, epoch)
 
         # train accuracy
         with torch.no_grad():
@@ -75,38 +93,46 @@ def train(img_dir, lbl_dir, weight_bit_width=1, act_bit_width=3, n_anchors=5, n_
             train_AP50 = 0.0
             train_AP75 = 0.0
             train_total = 0
-            for data in tqdm(train_loader, total=int(np.ceil(train_len/batch_size)), desc="train accuracy", unit="batch"):
+            for data in tqdm(
+                train_loader,
+                total=int(np.ceil(train_len / batch_size)),
+                desc="train accuracy",
+                unit="batch",
+            ):
                 images, labels = data[0].to(device), data[1].to(device)
                 outputs = net(images)
-                iou = IoU_calc(
-                    YOLOout(outputs.value.cpu(), anchors), labels.cpu())
+                iou = IoU_calc(YOLOout(outputs.value.cpu(), anchors), labels.cpu())
                 train_total += labels.size(0)
                 train_miou += iou.sum()
-                train_AP50 += (iou >= .5).sum()
-                train_AP75 += (iou >= .75).sum()
+                train_AP50 += (iou >= 0.5).sum()
+                train_AP75 += (iou >= 0.75).sum()
             # log accuracy statistics
-            logger.add_scalar('meanIoU/train', train_miou/train_total, epoch)
-            logger.add_scalar('meanAP50/train', train_AP50/train_total, epoch)
-            logger.add_scalar('meanAP75/train', train_AP75/train_total, epoch)
+            logger.add_scalar("meanIoU/train", train_miou / train_total, epoch)
+            logger.add_scalar("meanAP50/train", train_AP50 / train_total, epoch)
+            logger.add_scalar("meanAP75/train", train_AP75 / train_total, epoch)
         # test accuracy
         with torch.no_grad():
             test_miou = 0.0
             test_AP50 = 0.0
             test_AP75 = 0.0
             test_total = 0
-            for data in tqdm(test_loader, total=int(np.ceil(test_len/batch_size)), desc="test accuracy", unit="batch"):
+            for data in tqdm(
+                test_loader,
+                total=int(np.ceil(test_len / batch_size)),
+                desc="test accuracy",
+                unit="batch",
+            ):
                 images, labels = data[0].to(device), data[1].to(device)
                 outputs = net(images)
-                iou = IoU_calc(
-                    YOLOout(outputs.value.cpu(), anchors), labels.cpu())
+                iou = IoU_calc(YOLOout(outputs.value.cpu(), anchors), labels.cpu())
                 test_total += labels.size(0)
                 test_miou += iou.sum()
-                test_AP50 += (iou >= .5).sum()
-                test_AP75 += (iou >= .75).sum()
+                test_AP50 += (iou >= 0.5).sum()
+                test_AP75 += (iou >= 0.75).sum()
             # log accuracy statistics
-            logger.add_scalar('meanIoU/test', test_miou/test_total, epoch)
-            logger.add_scalar('meanAP50/test', test_AP50/test_total, epoch)
-            logger.add_scalar('meanAP75/test', test_AP75/test_total, epoch)
+            logger.add_scalar("meanIoU/test", test_miou / test_total, epoch)
+            logger.add_scalar("meanAP50/test", test_AP50 / test_total, epoch)
+            logger.add_scalar("meanAP75/test", test_AP75 / test_total, epoch)
 
     # save network
     path = f"./trained_net_W{weight_bit_width}A{act_bit_width}_a{n_anchors}_e{n_epochs}_b{batch_size}.pth"
