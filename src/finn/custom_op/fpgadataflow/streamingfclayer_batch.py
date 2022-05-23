833be8e12c054549324a3eebe89f3cfe689436da
Yaman Umuroglu <yamanu@xilinx.com>
2022-05-23 16:24:28 +0200

    [HLSCustomOp] generate separate sync/sim .dat files for decoupled

--- src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
+++ src/finn/custom_op/fpgadataflow/streamingfclayer_batch.py
@@ -818,16 +818,28 @@ class StreamingFCLayer_Batch(HLSCustomOp):
             self.make_weight_file(weights, "decoupled_npy", weight_filename_sim)
             if mem_mode == "decoupled":
                 # also save weights as Verilog .dat file
-                weight_filename_rtl = "{}/memblock_0.dat".format(code_gen_dir)
+                # note that we provide two different .dat files, one for synth
+                # and one for synthesis. this is because URAM-based weights always
+                # need zero weights for synthesis, otherwise they get inferred
+                # as BRAM
+                weight_filename_rtl_synth = "{}/memblock_synth_0.dat".format(
+                    code_gen_dir
+                )
+                weight_filename_rtl_sim = "{}/memblock_sim_0.dat".format(code_gen_dir)
+                # sim weights are always the true weights
+                self.make_weight_file(
+                    weights, "decoupled_verilog_dat", weight_filename_rtl_sim
+                )
                 ram_style = self.get_nodeattr("ram_style")
                 if ram_style == "ultra":
                     # UltraRAM must have no memory initializer, or only zeroes
                     # otherwise BRAM will be inferred instead of URAM
                     # as a workaround we provide a zero-weight init here
-                    # TODO handle this in Verilog with an if statement
-                    weights = np.zeros_like(weights)
+                    synth_weights = np.zeros_like(weights)
+                else:
+                    synth_weights = weights
                 self.make_weight_file(
-                    weights, "decoupled_verilog_dat", weight_filename_rtl
+                    synth_weights, "decoupled_verilog_dat", weight_filename_rtl_synth
                 )
         else:
             raise Exception(
