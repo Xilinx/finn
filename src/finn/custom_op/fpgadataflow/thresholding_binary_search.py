09c6da9fc27c3897d3a9cb7423a3e21978f17c2c
Fionn O'Donohoe <fionno@xilinx.com>
2022-09-21 15:36:12 +0100

    [thresholding] FINN-44: Add skeleton class for Threshold (the RTL version, no HLS support for this class required).
    The following functions have been removed when compared to the original Thresholding_Batch class:
        - get_weightstream_width_padded()
            needed for cppsim
        - get_ap_int_max_w()
            needed for cppsim
        - get_template_param_values()
            needed for cppsim
        - get_hls_compatible_threshold_tensor()
            needed for cppsim/hlslib
        - get_verilog_top_module_intf_names()
            already have TOP verilog module interface names I think
        - get_op_and_param_counts()
            not used anywhere
        - ipgen_extra_directives()
            needed for cppsim/hlslib
    
    Signed-off-by: Fionn O'Donohoe <fionno@xilinx.com>

---
+++ src/finn/custom_op/fpgadataflow/thresholding_binary_search.py
@@ -0,0 +1,159 @@
+# Copyright (c) 2020, Xilinx
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of FINN nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from finn.custom_op.fpgadataflow.hlscustomop import HLSCustomOp
+
+"""@package thresholding_binary_search
+- ONNX i/o tensor shape assumptions for Thresholding:
+- input 0 is the input tensor, shape (..., NumChannels)
+- input 1 is the threshold tensor, shape (NumChannels, n_thres)
+- output 0 is the output tensor, shape (..., NumChannels) - same as input
+- the '...' here can be any shape (representing groups of vectors)
+
+This module creates an RTL IP, HLS is not supported. See 'thresholding_batch'
+for a HLS equivalent.
+"""
+
+
+class Thresholding_Bin_Search(HLSCustomOp):
+    """Class that corresponds to finn-rtllib 'thresholding' function."""
+
+    def __init__(self, onnx_node):
+        super().__init__(onnx_node)
+
+    def get_nodeattr_types(self):
+        return {}
+
+    def calc_tmem(self):
+        return 0
+
+    def make_shape_compatible_op(self, model):
+        return []
+
+    def infer_node_datatype(self, model):
+        return
+
+    def verify_node(self):
+        return []
+
+    def bram_estimation(self):
+        return 0
+
+    def lut_estimation(self):
+        return 0
+
+    def get_input_datatype(self):
+        return None
+
+    def get_output_datatype(self):
+        return None
+
+    def get_weight_datatype(self):
+        return None
+
+    def minimize_accumulator_width(self, model):
+        return None
+
+    def get_instream_width(self):
+        return 0
+
+    def get_outstream_width(self):
+        return 0
+
+    def get_weightstream_width(self):
+        return 0
+
+    def get_folded_input_shape(self):
+        return tuple([] + [])
+
+    def get_folded_output_shape(self):
+        return tuple([] + [])
+
+    def get_normal_input_shape(self):
+        return tuple([] + [])
+
+    def get_normal_output_shape(self):
+        return tuple([] + [])
+
+    def get_number_output_values(self):
+        return 0
+
+    def get_exp_cycles(self):
+        return 0
+
+    def get_template_param_values(self):
+        return dict()
+
+    def make_weight_file(self, weights, weight_file_mode, weight_file_name):
+        """Produce a file containing given weights (thresholds) in appropriate
+        format for this layer. This file can be used for either synthesis or
+        run-time reconfig of weights.
+
+        Arguments:
+        * weights : numpy array with weights to be put into the file
+        * weight_file_mode : one of {hls_header, decoupled_verilog_dat,
+          decoupled_runtime}
+        * weight_file_name : filename for the weight file to be generated
+        """
+        return
+
+    def generate_params(self, model, path):
+        return
+
+    def execute_node(self, context, graph):
+        return
+
+    def code_generation_ipi(self):
+        return []
+
+    def global_includes(self):
+        pass
+
+    def defines(self, var):
+        pass
+
+    def read_npy_data(self):
+        pass
+
+    def strm_decl(self):
+        pass
+
+    def docompute(self):
+        pass
+
+    def dataoutstrm(self):
+        pass
+
+    def save_as_npy(self):
+        pass
+
+    def blackboxfunction(self):
+        pass
+
+    def pragmas(self):
+        pass
