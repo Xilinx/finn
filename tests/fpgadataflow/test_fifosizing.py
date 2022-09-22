deef462e9ecc8ef0c3b287eec986f9211e0dbca1
Yaman Umuroglu <maltanar@gmail.com>
2022-09-22 09:51:49 +0200

    [Test] add residual FIFO sizing testcase, currently fails

--- tests/fpgadataflow/test_fifosizing.py
+++ tests/fpgadataflow/test_fifosizing.py
@@ -27,6 +27,8 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+import pkg_resources as pk
+
 import pytest
 
 import json
@@ -78,3 +80,39 @@ def test_fifosizing_linear():
         > 0.9
     )
     shutil.rmtree(tmp_output_dir)
+
+
+@pytest.mark.slow
+@pytest.mark.vivado
+def test_fifosizing_residual():
+    model_fname = pk.resource_filename(
+        "finn.qnn-data", "testcase/residual_testcase.onnx"
+    )
+    tmp_output_dir = make_build_dir("build_fifosizing_residual")
+    cfg = build_cfg.DataflowBuildConfig(
+        output_dir=tmp_output_dir,
+        auto_fifo_depths=True,
+        auto_fifo_strategy="largefifo_rtlsim",
+        synth_clk_period_ns=10.0,
+        board="Pynq-Z1",
+        verbose=True,
+        rtlsim_batch_size=100,
+        verify_save_rtlsim_waveforms=True,
+        shell_flow_type=build_cfg.ShellFlowType.VIVADO_ZYNQ,
+        generate_outputs=[
+            build_cfg.DataflowOutputType.STITCHED_IP,
+            build_cfg.DataflowOutputType.RTLSIM_PERFORMANCE,
+            build_cfg.DataflowOutputType.ESTIMATE_REPORTS,
+        ],
+        default_mem_mode=build_cfg.ComputeEngineMemMode.DECOUPLED,
+    )
+    build.build_dataflow_cfg(model_fname, cfg)
+    with open(tmp_output_dir + "/report/estimate_network_performance.json") as f:
+        est_data = json.load(f)
+    with open(tmp_output_dir + "/report/rtlsim_performance.json") as f:
+        sim_data = json.load(f)
+    assert (
+        float(sim_data["throughput[images/s]"])
+        / float(est_data["estimated_throughput_fps"])
+        > 0.9
+    )
