5d07a435c2994f0238fb41ec21381d75ea049796
Fionn O'Donohoe <fionno@xilinx.com>
2023-03-31 10:45:34 +0100

    [thresholding]: add comment about why bipolar activations skipped for threhsolding binary search node
    Signed-off-by: Fionn O'Donohoe <fionno@xilinx.com>

--- tests/fpgadataflow/test_convert_to_hls_thresholding.py
+++ tests/fpgadataflow/test_convert_to_hls_thresholding.py
@@ -155,6 +155,9 @@ def test_convert_to_hls_tbs_rtl_variant(
     pe = generate_pe_value(fold, num_input_channels)
     num_steps = activation.get_num_possible_values() - 1
 
+    # See convert_to_hls_layers::InferThresholdingLayer:
+    # assert (not odt.signed()) or (actval < 0)
+    # This implies that it expects a negative activation, BIPOLAR does not provide that
     if activation == DataType["BIPOLAR"]:
         pytest.skip(
             "Only negative activations are supported for "
