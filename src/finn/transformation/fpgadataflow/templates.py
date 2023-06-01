aae59b1e6448c3274c36daa5667f943ee0d56a9a
Felix Jentzsch <felix.jentzsch@upb.de>
2023-06-01 11:18:40 +0200

    [Zynq build] update PS IP version

--- src/finn/transformation/fpgadataflow/templates.py
+++ src/finn/transformation/fpgadataflow/templates.py
@@ -135,7 +135,7 @@ if {$BOARD == "ZCU104"} {
 
 create_bd_design "top"
 if {$ZYNQ_TYPE == "zynq_us+"} {
-    create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.4 zynq_ps
+    create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.5 zynq_ps
     apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" }  [get_bd_cells zynq_ps]
     #activate one slave port, deactivate the second master port
     set_property -dict [list CONFIG.PSU__USE__S_AXI_GP2 {1}] [get_bd_cells zynq_ps]
