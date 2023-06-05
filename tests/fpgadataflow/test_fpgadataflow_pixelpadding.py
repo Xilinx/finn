dff63d3ef91b68f4cde5bb328ee75c3ee516e3fb
Hugo LE BLEVEC <hugo.leblevec@gmail.com>
2023-06-05 14:06:49 +0200

    Updating headers

--- tests/fpgadataflow/test_fpgadataflow_pixelpadding.py
+++ tests/fpgadataflow/test_fpgadataflow_pixelpadding.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, Xilinx
+# Copyright (c) 2023, Advanced Micro Devices, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -11,7 +11,7 @@
 #   this list of conditions and the following disclaimer in the documentation
 #   and/or other materials provided with the distribution.
 #
-# * Neither the name of FINN nor the names of its
+# * Neither the name of Xilinx nor the names of its
 #   contributors may be used to endorse or promote products derived from
 #   this software without specific prior written permission.
 #
