39c0c9d1979192cbc2b0c9688a5c5b8d4ff1f2cc
Yaman Umuroglu <yamanu@xilinx.com>
2023-06-22 08:21:09 +0000

    [QuantMaxNorm] iface naming

--- src/finn/custom_op/fpgadataflow/quantmaxnorm.py
+++ src/finn/custom_op/fpgadataflow/quantmaxnorm.py
@@ -155,23 +155,35 @@ class QuantMaxNorm(HLSCustomOp):
         npy_in = "%s/input_0.npy" % code_gen_dir
         self.code_gen_dict["$READNPYDATA$"] = []
         self.code_gen_dict["$READNPYDATA$"].append(
-            'npy2apintstream<%s, %s, %d, %s>("%s", in0);'
-            % (packed_hls_type, elem_hls_type, elem_bits, npy_type, npy_in)
+            'npy2apintstream<%s, %s, %d, %s>("%s", in0_%s);'
+            % (
+                packed_hls_type,
+                elem_hls_type,
+                elem_bits,
+                npy_type,
+                npy_in,
+                self.hls_sname(),
+            )
         )
 
     def strm_decl(self):
         self.code_gen_dict["$STREAMDECLARATIONS$"] = []
         self.code_gen_dict["$STREAMDECLARATIONS$"].append(
-            'hls::stream<ap_uint<{}>> in0 ("in0");'.format(self.get_instream_width())
+            'hls::stream<ap_uint<{}>> in0_{} ("in0_{}");'.format(
+                self.get_instream_width(), self.hls_sname(), self.hls_sname()
+            )
         )
         self.code_gen_dict["$STREAMDECLARATIONS$"].append(
-            'hls::stream<ap_uint<{}>> out ("out");'.format(self.get_outstream_width())
+            'hls::stream<ap_uint<{}>> out_{} ("out_{}");'.format(
+                self.get_outstream_width(), self.hls_sname(), self.hls_sname()
+            )
         )
 
     def docompute(self):
         self.code_gen_dict["$DOCOMPUTE$"] = [
             """max_norm<IFMDim, Input_precision, Output_precision>
-            (in0, out, SCALE0);"""
+            (in0_%s, out_%s, SCALE0);"""
+            % (self.hls_sname(), self.hls_sname())
         ]
 
     def dataoutstrm(self):
@@ -190,12 +202,13 @@ class QuantMaxNorm(HLSCustomOp):
         oshape_cpp_str = str(oshape).replace("(", "{").replace(")", "}")
 
         self.code_gen_dict["$DATAOUTSTREAM$"] = [
-            'apintstream2npy<%s, %s, %d, %s>(out, %s, "%s");'
+            'apintstream2npy<%s, %s, %d, %s>(out_%s, %s, "%s");'
             % (
                 packed_hls_type,
                 elem_hls_type,
                 elem_bits,
                 npy_type,
+                self.hls_sname(),
                 oshape_cpp_str,
                 npy_out,
             )
@@ -208,16 +221,24 @@ class QuantMaxNorm(HLSCustomOp):
         packed_hls_type_in = "ap_uint<%d>" % self.get_instream_width()
         packed_hls_type_out = "ap_uint<%d>" % self.get_outstream_width()
         self.code_gen_dict["$BLACKBOXFUNCTION$"] = [
-            "void %s(hls::stream<%s > &in0, hls::stream<%s > &out)"
-            % (self.onnx_node.name, packed_hls_type_in, packed_hls_type_out)
+            "void %s(hls::stream<%s > &in0_%s, hls::stream<%s > &out_%s)"
+            % (
+                self.onnx_node.name,
+                packed_hls_type_in,
+                self.hls_sname(),
+                packed_hls_type_out,
+                self.hls_sname(),
+            )
         ]
 
     def pragmas(self):
         self.code_gen_dict["$PRAGMAS$"] = [
-            "#pragma HLS INTERFACE axis port=in0 name=in0_" + self.hls_sname()
+            "#pragma HLS INTERFACE axis port=in0_%s name=in0_%s"
+            % (self.hls_sname(), self.hls_sname())
         ]
         self.code_gen_dict["$PRAGMAS$"].append(
-            "#pragma HLS INTERFACE axis port=out name=out_" + self.hls_sname()
+            "#pragma HLS INTERFACE axis port=out_%s name=out_%s"
+            % (self.hls_sname(), self.hls_sname())
         )
         self.code_gen_dict["$PRAGMAS$"].append(
             "#pragma HLS INTERFACE ap_ctrl_none port=return"
