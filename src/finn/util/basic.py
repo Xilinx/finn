039f122fb804693ab712bd6777581b3654a7ff0b
Yaman Umuroglu <yamanu@xilinx.com>
2023-12-04 18:20:51 +0000

    [Util] introduce new utilities for memory primitive alternatives

--- src/finn/util/basic.py
+++ src/finn/util/basic.py
@@ -30,6 +30,7 @@ import os
 import subprocess
 import sys
 import tempfile
+from qonnx.util.basic import roundup_to_integer_multiple
 
 # test boards
 test_board_map = ["Pynq-Z1", "KV260_SOM", "ZCU104", "U250"]
@@ -252,3 +253,48 @@ def find_next_power_of_2(n):
         # unset rightmost bit
         n = n & n - 1
     return n << 1
+
+
+mem_primitives_versal = {
+    "URAM_72x4096": (72, 4096),
+    "URAM_36x8192": (36, 8192),
+    "URAM_18x16384": (18, 16384),
+    "URAM_9x32768": (9, 32768),
+    "BRAM18_36x512": (36, 512),
+    "BRAM18_18x1024": (18, 1024),
+    "BRAM18_9x2048": (9, 2048),
+    "LUTRAM": (1, 64),
+}
+
+
+def get_memutil_alternatives(
+    req_mem_spec, mem_primitives=mem_primitives_versal, sort_min_waste=True
+):
+    ret = [
+        (primitive_name, memutil(req_mem_spec, primitive_spec))
+        for (primitive_name, primitive_spec) in mem_primitives.items()
+    ]
+    if sort_min_waste:
+        ret = sorted(ret, key=lambda x: x[1][2])
+    return ret
+
+
+def memutil(req_mem_spec, primitive_spec):
+    """Computes how many instances of a memory primitive are necessary to
+    implemented a desired memory size, where req_mem_spec is the desired
+    size and the primitive_spec is the primitve size. The sizes are expressed
+    as tuples of (mem_width, mem_depth). Returns (primitive_count, efficiency, waste)
+    where efficiency in range [0,1] indicates how much of the total capacity is
+    utilized, and waste indicates how many bits of storage are wasted."""
+
+    req_width, req_depth = req_mem_spec
+    prim_width, prim_depth = primitive_spec
+
+    match_width = roundup_to_integer_multiple(req_width, prim_width)
+    match_depth = roundup_to_integer_multiple(req_depth, prim_depth)
+    count_width = match_width // prim_width
+    count_depth = match_depth // prim_depth
+    count = count_depth * count_width
+    eff = (req_width * req_depth) / (count * prim_width * prim_depth)
+    waste = (count * prim_width * prim_depth) - (req_width * req_depth)
+    return (count, eff, waste)
