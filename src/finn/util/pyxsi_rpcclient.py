159e2286bff1da29deb2ad655220605b1e109406
Yaman Umuroglu <yamanu@xilinx.com>
2024-10-03 20:39:31 +0100

    [pyxsi] rework RPC interface to exclude rtlsim_multi_io

--- src/finn/util/pyxsi_rpcclient.py
+++ src/finn/util/pyxsi_rpcclient.py
@@ -32,12 +32,135 @@ import xmlrpc.client
 class PyXSIRPCProxy(object):
     def __init__(self, wrapped=None):
         if wrapped is None:
-            wrapped = xmlrpc.client.ServerProxy("http://localhost:8000")
+            wrapped = xmlrpc.client.ServerProxy("http://localhost:8000", allow_none=True)
         self.wrapped = wrapped
 
-    def __getattr__(self, name):
-        attr = getattr(self.wrapped, name)
-        return type(self)(attr)
+    def compile_sim_obj(self, top_module_name, source_list, sim_out_dir):
+        ret = self.wrapped.compile_sim_obj(top_module_name, source_list, sim_out_dir)
+        return ret
 
-    def __call__(self, *args, **kw):
-        return self.wrapped(*args, **kw)
+    def load_sim_obj(
+        self, sim_out_dir, out_so_relative_path, tracefile=None, is_toplevel_verilog=True
+    ):
+        ret = self.wrapped.load_sim_obj(
+            sim_out_dir, out_so_relative_path, tracefile, is_toplevel_verilog
+        )
+        return ret
+
+    def find_signal(self, sim_id, signal_name):
+        ret = self.wrapped.find_signal(sim_id, signal_name)
+        return ret
+
+    def read_signal(self, sim_id, signal_name):
+        signal_value_str = self.wrapped.read_signal(sim_id, signal_name)
+        signal_value = int(signal_value_str)
+        return signal_value
+
+    def write_signal(self, sim_id, signal_name, signal_value):
+        signal_value_str = str(signal_value)
+        self.wrapped.write_signal(sim_id, signal_name, signal_value_str)
+
+    def reset_rtlsim(self, sim_id, rst_name="ap_rst_n", active_low=True, clk_name="ap_clk"):
+        self.wrapped.reset_rtlsim(sim_id, rst_name, active_low, clk_name)
+
+    def toggle_clk(self, sim_id, clk_name="ap_clk"):
+        self.wrapped.toggle_clk(sim_id, clk_name)
+
+    def toggle_neg_edge(self, sim_id, clk_name="ap_clk"):
+        self.wrapped.toggle_neg_edge(sim_id, clk_name)
+
+    def toggle_pos_edge(self, sim_id, clk_name="ap_clk"):
+        self.wrapped.toggle_pos_edge(sim_id, clk_name)
+
+    def rtlsim_multi_io(
+        self,
+        sim_id,
+        io_dict,
+        num_out_values,
+        sname="_V_V_",
+        liveness_threshold=10000,
+        hook_preclk=None,
+        hook_postclk=None,
+    ):
+        for outp in io_dict["outputs"]:
+            self.write_signal(sim_id, outp + sname + "TREADY", 1)
+
+        # observe if output is completely calculated
+        # total_cycle_count will contain the number of cycles the calculation ran
+        output_done = False
+        total_cycle_count = 0
+        output_count = 0
+        old_output_count = 0
+
+        # avoid infinite looping of simulation by aborting when there is no change in
+        # output values after 100 cycles
+        no_change_count = 0
+
+        while not (output_done):
+            signals_to_write = {}
+            if hook_preclk:
+                hook_preclk(sim_id)
+            # Toggle falling edge to arrive at a delta cycle before the rising edge
+            self.toggle_neg_edge(sim_id)
+
+            # examine signals, decide how to act based on that but don't update yet
+            # so only read_signal access in this block, no _write_signal
+            for inp in io_dict["inputs"]:
+                inputs = io_dict["inputs"][inp]
+                signal_name = inp + sname
+                if (
+                    self.read_signal(sim_id, signal_name + "TREADY") == 1
+                    and self.read_signal(sim_id, signal_name + "TVALID") == 1
+                ):
+                    inputs = inputs[1:]
+                io_dict["inputs"][inp] = inputs
+
+            for outp in io_dict["outputs"]:
+                outputs = io_dict["outputs"][outp]
+                signal_name = outp + sname
+                if (
+                    self.read_signal(sim_id, signal_name + "TREADY") == 1
+                    and self.read_signal(sim_id, signal_name + "TVALID") == 1
+                ):
+                    outputs = outputs + [self.read_signal(sim_id, signal_name + "TDATA")]
+                    output_count += 1
+                io_dict["outputs"][outp] = outputs
+
+            # update signals based on decisions in previous block, but don't examine anything
+            # so only write_signal access in this block, no read_signal
+            for inp in io_dict["inputs"]:
+                inputs = io_dict["inputs"][inp]
+                signal_name = inp + sname
+                signals_to_write[signal_name + "TVALID"] = 1 if len(inputs) > 0 else 0
+                signals_to_write[signal_name + "TDATA"] = inputs[0] if len(inputs) > 0 else 0
+
+            # Toggle rising edge to arrive at a delta cycle before the falling edge
+            self.toggle_pos_edge(sim_id)
+
+            for k, v in signals_to_write.items():
+                self.write_signal(sim_id, k, v)
+
+            if hook_postclk:
+                hook_postclk(sim_id)
+
+            total_cycle_count = total_cycle_count + 1
+
+            if output_count == old_output_count:
+                no_change_count = no_change_count + 1
+            else:
+                no_change_count = 0
+                old_output_count = output_count
+
+            # check if all expected output words received
+            if output_count == num_out_values:
+                output_done = True
+
+            # end sim on timeout
+            if no_change_count == liveness_threshold:
+                raise Exception(
+                    "Error in simulation! Takes too long to produce output. "
+                    "Consider setting the liveness_threshold parameter to a "
+                    "larger value."
+                )
+
+        return total_cycle_count
--- src/finn/util/pyxsi_rpcserver.py
+++ src/finn/util/pyxsi_rpcserver.py
@@ -42,15 +42,84 @@ except KeyError:
     assert False, "Must be launched with LD_LIBRARY_PATH=$(XILINX_VIVADO)/lib/lnx64.o"
 
 
-# Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ("/RPC2",)
 
 
+# we need to do some conversions while pyxsi calls are going through xmlrpc:
+# * sim objs become strings (stored in the sim_id_to_obj dict until done)
+# * signal values become strings
+#   (converted back and forth to Python integers)
+
+sim_id_to_obj = {}
+
+
+def compile_sim_obj(top_module_name, source_list, sim_out_dir):
+    ret = pyxsi_utils.compile_sim_obj(top_module_name, source_list, sim_out_dir)
+    return ret
+
+
+def load_sim_obj(sim_out_dir, out_so_relative_path, tracefile=None, is_toplevel_verilog=True):
+    ret_sim_obj = pyxsi_utils.load_sim_obj(
+        sim_out_dir, out_so_relative_path, tracefile, is_toplevel_verilog
+    )
+    ret_sim_id = str(id(ret_sim_obj))
+    sim_id_to_obj[ret_sim_id] = ret_sim_obj
+    return ret_sim_id
+
+
+def find_signal(sim_id, signal_name):
+    sim = sim_id_to_obj[sim_id]
+    return pyxsi_utils._find_signal(sim, signal_name)
+
+
+def read_signal(sim_id, signal_name):
+    sim = sim_id_to_obj[sim_id]
+    signal_value = pyxsi_utils._read_signal(sim, signal_name)
+    signal_value_str = str(signal_value)
+    return signal_value_str
+
+
+def write_signal(sim_id, signal_name, signal_value_str):
+    sim = sim_id_to_obj[sim_id]
+    signal_value = int(signal_value_str)
+    pyxsi_utils._write_signal(sim, signal_name, signal_value)
+
+
+def reset_rtlsim(sim_id, rst_name, active_low, clk_name):
+    sim = sim_id_to_obj[sim_id]
+    pyxsi_utils.reset_rtlsim(sim, rst_name, active_low, clk_name)
+
+
+def toggle_clk(sim_id, clk_name):
+    sim = sim_id_to_obj[sim_id]
+    pyxsi_utils.toggle_clk(sim, clk_name)
+
+
+def toggle_neg_edge(sim_id, clk_name):
+    sim = sim_id_to_obj[sim_id]
+    pyxsi_utils.toggle_neg_edge(sim, clk_name)
+
+
+def toggle_pos_edge(sim_id, clk_name):
+    sim = sim_id_to_obj[sim_id]
+    pyxsi_utils.toggle_pos_edge(sim, clk_name)
+
+
 # Create server
 port = 8000
-with SimpleXMLRPCServer(("localhost", port), requestHandler=RequestHandler) as server:
+with SimpleXMLRPCServer(
+    ("localhost", port), requestHandler=RequestHandler, allow_none=True
+) as server:
     server.register_introspection_functions()
-    server.register_instance(pyxsi_utils)
+    server.register_function(compile_sim_obj)
+    server.register_function(load_sim_obj)
+    server.register_function(find_signal)
+    server.register_function(read_signal)
+    server.register_function(write_signal)
+    server.register_function(reset_rtlsim)
+    server.register_function(toggle_clk)
+    server.register_function(toggle_neg_edge)
+    server.register_function(toggle_pos_edge)
     print(f"pyxsi RPC server is now running at {port}")
     server.serve_forever()
