664eaa1c4b344530daff46de9fe349701920b9ba
aziz bahri <azizb@amd.com>
2024-11-24 16:35:22 +0000

    DynMVAU: stub hdl generation functions
    - These functions are specific to dynamic mm

--- src/finn/custom_op/fpgadataflow/rtl/dynmvau_rtl.py
+++ src/finn/custom_op/fpgadataflow/rtl/dynmvau_rtl.py
@@ -3,3 +3,10 @@ from finn.custom_op.fpgadataflow.rtl.matrixvectoractivation_rtl import MVAU_rtl
 class DynMVAU_rtl(MVAU_rtl):
     def __init__(self, onnx_node, **kwargs):
         super().__init__(onnx_node, **kwargs)
+
+    def generate_params(self, model, path):
+        # Dynamic MVAU does not have weight parameters
+        pass
+
+    def generate_hdl(self, model, fpgapart, clk):
+        pass
