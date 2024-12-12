6de05a08928037e3f4ea527fa867407daaa890d6
auphelia <jakobapk@web.de>
2024-12-12 14:30:24 +0000

    [DuplicateStreams] Relax constraints on datatype for hw conversion function

--- src/finn/transformation/fpgadataflow/convert_to_hw_layers.py
+++ src/finn/transformation/fpgadataflow/convert_to_hw_layers.py
@@ -531,8 +531,7 @@ class InferDuplicateStreamsLayer(Transformation):
         graph_modified = False
         # check first if global input is split
         successors = model.find_consumers(graph.input[0].name)
-        dt = model.get_tensor_datatype(graph.input[0].name)
-        if successors is not None and len(successors) >= 2 and dt.is_integer():
+        if successors is not None and len(successors) >= 2:
             output_tensor = graph.input[0].name
             n_outputs = len(successors)
             dt = model.get_tensor_datatype(output_tensor)
