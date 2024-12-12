073a5ded38846df1bed14bc65a544a4632a8a480
auphelia <jakobapk@web.de>
2024-12-12 15:07:42 +0000

    [Tests] Add streamlining test to move scalar mul past dynamic matmul

--- tests/transformation/streamline/test_move_scalar_past_matmul.py
+++ tests/transformation/streamline/test_move_scalar_past_matmul.py
@@ -73,6 +73,37 @@ def test_move_scalar_mul_past_matmul():
 
 
 @pytest.mark.streamline
+def test_move_scalar_mul_past_dyn_matmul():
+    top_in0 = oh.make_tensor_value_info("top_in0", TensorProto.FLOAT, [1, 2])
+    top_in1 = oh.make_tensor_value_info("top_in1", TensorProto.FLOAT, [2, 2])
+    mul_param = oh.make_tensor_value_info("mul_param", TensorProto.FLOAT, [1, 1])
+    top_out = oh.make_tensor_value_info("top_out", TensorProto.FLOAT, [1, 2])
+    modelproto = qonnx_make_model(
+        oh.make_graph(
+            name="test",
+            inputs=[top_in0, top_in1],
+            outputs=[top_out],
+            value_info=[mul_param],
+            nodes=[
+                oh.make_node("Mul", ["top_in0", "mul_param"], ["middle"]),
+                oh.make_node("MatMul", ["middle", "top_in1"], ["top_out"]),
+            ],
+        )
+    )
+    model = ModelWrapper(modelproto)
+    model = model.transform(InferShapes())
+    model.set_initializer("mul_param", np.asarray([[3]], dtype=np.float32))
+    new_model = model.transform(MoveScalarMulPastMatMul())
+    inp_val0 = np.asarray([[-1.0, 1.0]], dtype=np.float32)
+    inp_val1 = np.asarray([[2, 4], [-1, 1]], dtype=np.float32)
+    inp_dict = {"top_in0": inp_val0, "top_in1": inp_val1}
+    assert ox.compare_execution(model, new_model, inp_dict)
+    assert new_model.graph.node[0].op_type == "MatMul"
+    assert new_model.graph.node[1].op_type == "Mul"
+    assert new_model.graph.node[0].output[0] == new_model.graph.node[1].input[0]
+
+
+@pytest.mark.streamline
 def test_move_scalar_add_past_matmul():
     top_in = oh.make_tensor_value_info("top_in", TensorProto.FLOAT, [1, 2])
     add_param = oh.make_tensor_value_info("add_param", TensorProto.FLOAT, [1, 1])
