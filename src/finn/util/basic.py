ba0261fd2d431568917f1ece7f8569da2daf14ec
jsmonson <jsmonson@gmail.com>
2025-01-13 09:32:00 -0700

    add corrected spacing

--- src/finn/util/basic.py
+++ src/finn/util/basic.py
@@ -83,6 +83,7 @@ part_map["VEK280"] = "xcve2802-vsvh1760-2MP-e-S"
 part_map["VCK190"] = "xcvc1902-vsva2197-2MP-e-S"
 part_map["V80"] = "xcv80-lsva4737-2MHP-e-s"
 
+
 def get_rtlsim_trace_depth():
     """Return the trace depth for rtlsim via PyVerilator. Controllable
     via the RTLSIM_TRACE_DEPTH environment variable. If the env.var. is
