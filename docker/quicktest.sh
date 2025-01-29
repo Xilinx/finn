843759424d4c54a18036dca9a16f8e1f5a5a74df
Felix Jentzsch <45395194+fpjentzsch@users.noreply.github.com>
2025-01-29 22:27:20 +0100

    CI for Test Suite (#4)
    * Add CI for syncing finn-dev, Singularity build, quicktest
    
    * Extend CI to full test suite, workaround for onnxruntime on slurm
    
    * Run test suite on 2024.1 in parallel
    
    * Use exclusive nodes for test suite
    
    * Try pytest forked option
    
    * Make slurm partition/qos configurable
    
    * Adjust pipeline triggers
    
    * Fix CI definition
    
    * Do not keep every Singularity image
    
    * Fix .sif naming
    
    * Apptainer build with force

--- docker/quicktest.sh
+++ docker/quicktest.sh
@@ -4,23 +4,25 @@
 
 cd $FINN_ROOT
 # check if command line argument is empty or not present
-if [ -z $1 ]; then
+if [ -z $1 ] || [ $1 = "quicktest" ]; then
   echo "Running quicktest: not (vivado or slow or board) with pytest-xdist"
-  pytest -m 'not (vivado or slow or vitis or board or notebooks or bnn_pynq)' --dist=loadfile -n $PYTEST_PARALLEL
+  pytest -m 'not (vivado or slow or vitis or board or notebooks or bnn_pynq)' --forked --dist=loadfile -n $PYTEST_PARALLEL
 elif [ $1 = "main" ]; then
   echo "Running main test suite: not (rtlsim or end2end) with pytest-xdist"
-  pytest -k 'not (rtlsim or end2end)' --dist=loadfile -n $PYTEST_PARALLEL
+  pytest -k 'not (rtlsim or end2end)' --forked --dist=loadfile -n $PYTEST_PARALLEL
 elif [ $1 = "rtlsim" ]; then
   echo "Running rtlsim test suite with pytest-parallel"
-  pytest -k rtlsim --workers $PYTEST_PARALLEL
+# there are end2end tests which also have rtlsim in their name, but not vice versa
+  pytest -k 'rtlsim and not end2end' --forked --workers $PYTEST_PARALLEL
 elif [ $1 = "end2end" ]; then
   echo "Running end2end test suite with no parallelism"
-  pytest -k end2end
+# filtering by name "end2end" is not sufficient, as the bnn tests need to be selected by a marker
+  pytest -k end2end --forked -m 'sanity_bnn or end2end or fpgadataflow or notebooks'
 elif [ $1 = "full" ]; then
   echo "Running full test suite, each step with appropriate parallelism"
   $0 main;
   $0 rtlsim;
   $0 end2end;
 else
-  echo "Unrecognized argument to quicktest.sh"
+  echo "Unrecognized argument to quicktest.sh: $1"
 fi
