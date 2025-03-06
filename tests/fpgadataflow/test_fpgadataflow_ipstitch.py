7475b2d7f7f02c60fa52214d014d074456d0b5fd
auphelia <jakobapk@web.de>
2025-03-06 17:07:37 +0000

    [EnvVar] Remove unused ALVEO and PYNQ env vars

--- tests/fpgadataflow/test_fpgadataflow_ipstitch.py
+++ tests/fpgadataflow/test_fpgadataflow_ipstitch.py
@@ -56,7 +56,7 @@ from finn.util.basic import alveo_default_platform, alveo_part_map, pynq_part_ma
 from finn.util.pyverilator import pyverilate_stitched_ip
 from finn.util.test import load_test_checkpoint_or_skip
 
-test_pynq_board = os.getenv("PYNQ_BOARD", default="Pynq-Z1")
+test_pynq_board = "Pynq-Z1"
 test_fpga_part = pynq_part_map[test_pynq_board]
 
 ip_stitch_model_dir = os.environ["FINN_BUILD_DIR"]
