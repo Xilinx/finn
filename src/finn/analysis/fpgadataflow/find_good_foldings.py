3a3577f82780727021cf74d2fe597241fe56d3a4
Yaman Umuroglu <maltanar@gmail.com>
2025-03-13 00:15:20 +0100

    [Analysis, Build] try to find + report "good" folding configs

---
+++ src/finn/analysis/fpgadataflow/find_good_foldings.py
@@ -0,0 +1,66 @@
+# Copyright (C) 2025, Advanced Micro Devices, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of FINN nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from functools import reduce
+from itertools import product
+
+
+def possible_targets(node_possible_configs):
+    return set([x[1] for x in node_possible_configs[list(node_possible_configs.keys())[0]]])
+
+
+def targets_to_interval(targets, rtol):
+    return [(x, x + rtol * x) for x in sorted(targets)]
+
+
+def interval_intersect(set_a, set_b):
+    res = []
+    all_combs = product(set_a, set_b)
+    for ea, eb in all_combs:
+        eo = (max(ea[0], eb[0]), min(ea[1], eb[1]))
+        if eo[0] <= eo[1]:
+            res.append(eo)
+    return res
+
+
+def find_candidate_intervals(possible_foldings_dict, rtol):
+    possible_target_intervals = [
+        targets_to_interval(possible_targets(v), rtol=rtol)
+        for k, v in possible_foldings_dict.items()
+    ]
+    candidate_intervals = reduce(interval_intersect, possible_target_intervals)
+    candidate_intervals = set([x[0] for x in candidate_intervals])
+    return sorted(list(candidate_intervals))
+
+
+def find_good_foldings(possible_foldings, rtol=0.4):
+    vau_only = {k: v for k, v in possible_foldings.items() if "VAU" in k}
+    return find_candidate_intervals(vau_only, rtol)
+
+
+# TODO wrap the above into a proper analysis pass
--- src/finn/builder/build_dataflow_steps.py
+++ src/finn/builder/build_dataflow_steps.py
@@ -57,6 +57,7 @@ import finn.transformation.fpgadataflow.convert_to_hw_layers as to_hw
 import finn.transformation.streamline.absorb as absorb
 from finn.analysis.fpgadataflow.dataflow_performance import dataflow_performance
 from finn.analysis.fpgadataflow.exp_cycles_per_layer import exp_cycles_per_layer
+from finn.analysis.fpgadataflow.find_good_foldings import find_good_foldings
 from finn.analysis.fpgadataflow.hls_synth_res_estimation import hls_synth_res_estimation
 from finn.analysis.fpgadataflow.op_and_param_counts import (
     aggregate_dict_keys,
@@ -422,13 +423,30 @@ def step_target_fps_parallelization(model: ModelWrapper, cfg: DataflowBuildConfi
 
     target_cycles_per_frame = cfg._resolve_cycles_per_frame()
     if target_cycles_per_frame is not None:
-        model = model.transform(
-            SetFolding(
-                target_cycles_per_frame,
-                mvau_wwidth_max=cfg.mvau_wwidth_max,
-                two_pass_relaxation=cfg.folding_two_pass_relaxation,
-            )
+        model = model.transform(GiveUniqueNodeNames())
+        folding_trf = SetFolding(
+            target_cycles_per_frame,
+            mvau_wwidth_max=cfg.mvau_wwidth_max,
+            two_pass_relaxation=cfg.folding_two_pass_relaxation,
         )
+        model = model.transform(folding_trf)
+        report_dir = cfg.output_dir + "/report"
+        os.makedirs(report_dir, exist_ok=True)
+        with open(report_dir + "/possible_foldings.json", "w") as f:
+            json.dump(folding_trf.possible_foldings, f, indent=2)
+        rtols = [0.1, 0.2, 0.4, 0.8]
+        with open(report_dir + "/good_foldings.txt", "w") as f:
+            for rtol in rtols:
+                good_foldings = find_good_foldings(folding_trf.possible_foldings, rtol=rtol)
+                good_foldings_lst = [f"II\t\tFPS@{cfg._resolve_hls_clk_period()}ns"]
+                ticks_per_s = 10**9 / cfg._resolve_hls_clk_period()
+                good_foldings_lst += [f"{x}\t\t{ticks_per_s/x}" for x in good_foldings]
+                f.write(
+                    f"Folding targets where MVU/VVU nodes are at most {rtol*100}% "
+                    "faster than target:\n\n"
+                )
+                f.write("\n".join(good_foldings_lst) + "\n\n")
+
         # extract the suggested configuration and save it as json
         hw_attrs = [
             "PE",
