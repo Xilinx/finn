e2dc64349326f8b7b44f87132b50510a55cb95d2
auphelia <jakobapk@web.de>
2025-03-27 11:10:30 +0000

    [memstream] Update copyright header

--- finn-rtllib/memstream/hdl/memstream.sv
+++ finn-rtllib/memstream/hdl/memstream.sv
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2023, Xilinx
+ * Copyright (c) 2023, Advanced Micro Devices, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
