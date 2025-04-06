b5ef62e780a92059a230af7b9c256e4bb658e3be
Yaman Umuroglu <yamanu@xilinx.com>
2025-04-06 22:18:03 +0100

    [Frontend] bring back rescale=0.05

--- src/finn/builder/frontend_steps.py
+++ src/finn/builder/frontend_steps.py
@@ -177,6 +177,7 @@ def step_convert_to_thresholds_new(model: ModelWrapper, cfg: DataflowBuildConfig
 
     trn = QuantToMultiThreshold(
         range_info=cfg.input_range_info[0],
+        rescale=0.05,
         assume_monotonic=True,
         quant_filter=default_filter_function_generator(
             max_multithreshold_bit_width=cfg.max_multithreshold_bit_width
