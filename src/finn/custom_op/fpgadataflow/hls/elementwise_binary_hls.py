443f6eec332f85db88f3e00319bdac7470aadadd
Yaman Umuroglu <maltanar@gmail.com>
2025-05-03 16:41:05 +0200

    [Eltwise] workaround: manually update attrs for EltwiseFloat2IntCast
    running into a strange bug with cppsim execution otherwise

--- src/finn/custom_op/fpgadataflow/hls/elementwise_binary_hls.py
+++ src/finn/custom_op/fpgadataflow/hls/elementwise_binary_hls.py
@@ -820,11 +820,27 @@ class ElementwiseFloat2Int_hls(  # noqa: Class name does not follow
 
     # we need to resolve the attribute types due to multiple inheritence
     def get_nodeattr_types(self):
+        # TODO ran into problems resolving node attributes during Python exec,
+        # so specifying manually for now - why don't the super() calls work as expected?
         # start with attributes from ElementwiseBinaryOperation
-        attrs = super(ElementwiseBinaryOperation_hls, self).get_nodeattr_types()
+        attrs = super(ElementwiseBinaryOperation, self).get_nodeattr_types()
+        # Add the HLSBackend default attributes on top
+        attrs.update(HLSBackend.get_nodeattr_types(self))
+        # Add/Specialize implementation specific attributes here...
+        # Return the updated attributes dictionary
         # add attributes from ElementwiseFloat2Int
         attrs_float2int = super(elementwise_binary.ElementwiseFloat2Int, self).get_nodeattr_types()
         attrs.update(attrs_float2int)
+        attrs.update({
+            # Bitwidth of output integers
+            "bitwidth": ("i", True, 0),
+            # Whether output integers are signed or unsigned
+            "signed": ("i", True, 0),
+            # Whether output integers use narrow-range
+            "narrow": ("i", True, 0),
+            # The rounding mode, which is used for the quant function
+            "rounding_mode": ("s", True, "ROUND"),
+        })
         # Return updated attribute dictionary
         return attrs
 
