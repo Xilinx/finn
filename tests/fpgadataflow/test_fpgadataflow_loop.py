3b6c25898ae577dc4a850740e623ab0284227ba5
Shane Fleming <sfleming@xilinx.com>
2025-06-26 12:46:18 +0100

    Added an extra make_model function to create a simpler MLO model with no fork

--- tests/fpgadataflow/test_fpgadataflow_loop.py
+++ tests/fpgadataflow/test_fpgadataflow_loop.py
@@ -22,7 +22,8 @@ from finn.util.basic import part_map
 test_board = "V80"
 test_fpga_part = part_map[test_board]
 
-ip_stitch_model_dir = os.environ["FINN_BUILD_DIR"]
+#ip_stitch_model_dir = os.environ["FINN_BUILD_DIR"]
+ip_stitch_model_dir = "./tmpbuild" 
 
 def generate_random_threshold_values(data_type, num_input_channels, num_steps):
     return np.random.randint(
@@ -289,6 +290,164 @@ def make_loop_modelwrapper(mw, mh, iter_count):
     return model
 
 
+def make_loop_modelwrapper_nofork(mw, mh, iter_count):
+    ifm = helper.make_tensor_value_info("ifm", TensorProto.FLOAT, [1, 3, 3, mw])
+    mm0_out = helper.make_tensor_value_info("mm0_out", TensorProto.FLOAT, [1, 3, 3, mh])
+    mt0_out = helper.make_tensor_value_info("mt0_out", TensorProto.FLOAT, [1, 3, 3, mh])
+    mm1_out = helper.make_tensor_value_info("mm1_out", TensorProto.FLOAT, [1, 3, 3, mh])
+    mt1_out = helper.make_tensor_value_info("mt1_out", TensorProto.FLOAT, [1, 3, 3, mh])
+    ofm = helper.make_tensor_value_info("ofm", TensorProto.FLOAT, (1, 3, 3, mh))
+    dtype = DataType["INT8"]
+    W0 = gen_finn_dt_tensor(dtype, (mw, mh))
+    W1 = gen_finn_dt_tensor(dtype, (mw, mh))
+    T0 = generate_random_threshold_values(dtype, 1, dtype.get_num_possible_values() - 1)
+    T0 = np.sort(T0, axis=1)
+    T1 = generate_random_threshold_values(dtype, 1, dtype.get_num_possible_values() - 1)
+    T1 = np.sort(T1, axis=1)
+    weights0 = helper.make_tensor_value_info("weights0", TensorProto.FLOAT, [mw, mh])
+    weights1 = helper.make_tensor_value_info("weights1", TensorProto.FLOAT, [mw, mh])
+    thresh0 = helper.make_tensor_value_info("thresh0", TensorProto.FLOAT, T0.shape)
+    thresh1 = helper.make_tensor_value_info("thresh1", TensorProto.FLOAT, T1.shape)
+
+    matmul_node0 = helper.make_node(
+        "MVAU_rtl",
+        ["ifm", "weights0"],
+        ["mm0_out"],
+        domain="finn.custom_op.fpgadataflow.rtl",
+        backend="fpgadataflow",
+        MW=mw,
+        MH=mh,
+        SIMD=1,
+        PE=1,
+        inputDataType="INT8",
+        weightDataType="INT8",
+        outputDataType="INT32",
+        ActVal=0,
+        binaryXnorMode=0,
+        noActivation=1,
+        numInputVectors=list((1, 3, 3)),
+        mlo_max_iter=3,
+        inFIFODepths=[2, 2],
+        name="MVAU_rtl0",
+    )
+    mt_node0 = helper.make_node(
+        "Thresholding_rtl",
+        ["mm0_out", "thresh0"],
+        ["mt0_out"],
+        domain="finn.custom_op.fpgadataflow.rtl",
+        backend="fpgadataflow",
+        NumChannels=mh,
+        PE=1,
+        numSteps=T0.shape[1],
+        inputDataType="INT32",
+        weightDataType="INT33",
+        outputDataType="INT8",
+        numInputVectors=list((1, 3, 3)),
+        mlo_max_iter=3,
+        inFIFODepths=[2, 2],
+        ActVal=int(dtype.min()),
+        name="Thresholding_rtl0",
+    )
+    matmul_node1 = helper.make_node(
+        "MVAU_rtl",
+        ["mt0_out", "weights1"],
+        ["mm1_out"],
+        domain="finn.custom_op.fpgadataflow.rtl",
+        backend="fpgadataflow",
+        MW=mw,
+        MH=mh,
+        SIMD=1,
+        PE=1,
+        inputDataType="INT8",
+        weightDataType="INT8",
+        outputDataType="INT32",
+        ActVal=0,
+        binaryXnorMode=0,
+        noActivation=1,
+        numInputVectors=list([1, 3, 3]),
+        mlo_max_iter=3,
+        inFIFODepths=[2, 2],
+        name="MVAU_rtl1",
+    )
+    mt_node1 = helper.make_node(
+        "Thresholding_rtl",
+        ["mm1_out", "thresh1"],
+        ["ofm"],
+        domain="finn.custom_op.fpgadataflow.rtl",
+        backend="fpgadataflow",
+        NumChannels=mh,
+        PE=1,
+        numSteps=T1.shape[1],
+        inputDataType="INT32",
+        weightDataType="INT33",
+        outputDataType="INT8",
+        numInputVectors=list((1, 3, 3)),
+        mlo_max_iter=3,
+        inFIFODepths=[2, 2],
+        ActVal=int(dtype.min()),
+        name="Thresholding_rtl1",
+    )
+
+    nodes = [
+        matmul_node0,
+        mt_node0,
+        matmul_node1,
+        mt_node1
+    ]
+    loop_body = helper.make_graph(
+        nodes=nodes,
+        name="matmul_graph",
+        inputs=[ifm, weights0, thresh0, weights1, thresh1],
+        outputs=[ofm],
+        value_info=[mm0_out, mt0_out, mm1_out, mt1_out],
+    )
+    loop_body_model = qonnx_make_model(loop_body, producer_name="loop-body-model")
+    loop_body_model = ModelWrapper(loop_body_model)
+
+    loop_body_model.set_tensor_datatype("weights0", dtype)
+    loop_body_model.set_tensor_datatype("weights1", dtype)
+    loop_body_model.set_tensor_datatype("thresh0", dtype)
+    loop_body_model.set_tensor_datatype("thresh1", dtype)
+    loop_body_model.set_tensor_datatype("ifm", dtype)
+    loop_body_model.set_tensor_datatype("ofm", dtype)
+    loop_body_model = loop_body_model.transform(InferShapes())
+    loop_body_model = loop_body_model.transform(InferDataTypes())
+
+    iteration = 3
+    # stack according to iteration count
+    W0 = np.stack([W0] * iter_count)
+    W1 = np.stack([W1] * iter_count)
+    T0 = np.stack([T0] * iter_count)
+    T1 = np.stack([T1] * iter_count)
+    loop_node = helper.make_node(
+        "FINNLoop",
+        name="FINNLoop_0",
+        domain="finn.custom_op.fpgadataflow.rtl",
+        backend="fpgadataflow",
+        inputs=["ifm", "weights0", "thresh0", "weights1", "thresh1"],
+        outputs=["ofm"],
+        body=loop_body_model.graph,
+        iteration=iteration,
+        inputDataType="INT8",
+        outputDataType="INT8",
+    )
+    graph = helper.make_graph(nodes=[loop_node], name="loop_graph", inputs=[ifm], outputs=[ofm])
+    model = qonnx_make_model(graph, producer_name="fclayer-model")
+    model = ModelWrapper(model)
+
+    model.set_initializer("weights0", W0)
+    model.set_tensor_datatype("weights0", dtype)
+    model.set_initializer("weights1", W1)
+    model.set_tensor_datatype("weights1", dtype)
+    model.set_initializer("thresh0", T0)
+    model.set_tensor_datatype("thresh0", dtype)
+    model.set_initializer("thresh1", T1)
+    model.set_tensor_datatype("thresh1", dtype)
+    model.set_tensor_datatype("ifm", dtype)
+    model.set_tensor_datatype("ofm", dtype)
+
+    return model
+
 def test_fpgadataflow_loop():
     model = make_loop_modelwrapper(16, 16, 3)
     model = model.transform(InferShapes())
@@ -324,7 +483,7 @@ def test_fpgadataflow_loop():
 @pytest.mark.vivado
 def test_fpgadataflow_loop_stitchedip():
     """ Attemptes to make a stitchedIP of the loop body """
-    model = make_loop_modelwrapper(16,16,3)
+    model = make_loop_modelwrapper_nofork(16,16,3)
     model = model.transform(InferShapes())
     model.save("finn_loop_sip.onnx")
     inst = getCustomOp(model.graph.node[0])
@@ -332,4 +491,6 @@ def test_fpgadataflow_loop_stitchedip():
     body = body.transform(PrepareIP(test_fpga_part, 5))
     body = body.transform(HLSSynthIP())
     body = body.transform(CreateStitchedIP(test_fpga_part, 5))
+    #inst.set_nodeattr("body", body.graph)
+    #model = model.transform(CreateStitchedIP(test_fpga_part, 5))
     body.save("post_loop_stitched_ip.onnx")
