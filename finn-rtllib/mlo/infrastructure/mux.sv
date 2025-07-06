ebd0721e32dde77530afd149743a3a15e148d1c7
Joshua Monson <joshmonson@microsoft.com>
2025-07-06 00:46:18 +0000

    prior code was dropping first piece of data.

--- finn-rtllib/mlo/infrastructure/mux.sv
+++ finn-rtllib/mlo/infrastructure/mux.sv
@@ -123,8 +123,8 @@ assign axis_fs_tready = m_axis_int_tready;
 always_comb begin: DP_GEN
     cnt_gen_N = cnt_gen_C;
 
-    axis_fs_tvalid = 1'b0;
-    axis_fs_tdata = s_axis_fs_tdata;
+    axis_fs_tvalid = s_axis_fs_tvalid;
+    axis_fs_tdata  = s_axis_fs_tdata;
     //s_axis_fs_tready = 1'b0;
 
     idx_fs_tvalid = 1'b0;
@@ -138,7 +138,7 @@ always_comb begin: DP_GEN
         end
 
         ST_GEN_DATA: begin
-            axis_fs_tvalid = s_axis_fs_tvalid;
+            //axis_fs_tvalid = s_axis_fs_tvalid;
             //s_axis_fs_tready = axis_fs_tready;
 
             if(s_axis_fs_tvalid && s_axis_fs_tready) begin
