df2664a9df63bb0b4ea93a338be255436ba1c396
Simon Gerber <simon.gerber@amd.com>
2026-04-22 09:46:59 +0100

    [Compressor Documentation] Documentation updates and extended UltraScale+ test coverage

--- src/finn/compressor/src/dotp.py
+++ src/finn/compressor/src/dotp.py
@@ -27,6 +27,10 @@ if __name__ == "__main__":
 	if target_arg == "7series":
 		target = SevenSeries()
 		fpga_part = "xc7z020clg400-1"
+	elif target_arg == "ultrascale":
+		from .target import UltraScale
+		target = UltraScale()
+		fpga_part = "xczu9eg-ffvb1156-2-e"
 	else:  # versal (default)
 		target = Versal()
 		fpga_part = "xcvc1902-vsva2197-2MP-e-S"
