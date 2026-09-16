eee41eb70ed094ad6c818926b4d3ae19af47adf8
auphelia <jakobapk@web.de>
2026-09-16 15:40:32 +0100

    [Tests] Remove obsolete vivado marker from builder checks test

--- tests/util/test_build_dataflow_checks.py
+++ tests/util/test_build_dataflow_checks.py
@@ -44,7 +44,6 @@ def cfg(output_dir, **kw):
 
 
 @pytest.mark.util
-@pytest.mark.vivado
 class TestConfigCheckIntegration:
     def test_report_files_created(self):
         """Config check report should be saved to output_dir."""
