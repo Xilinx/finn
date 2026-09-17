4e3224f12ebe6a2d079574bbd71361b587b7ef40
Thomas B. Preußer <thomas.preusser@xilinx.com>
2026-09-17 11:43:27 +0100

    Pro-forma folding config to prevent missing reports.

--- tests/fpgadataflow/test_fpgadataflow_finnloop.py
+++ tests/fpgadataflow/test_fpgadataflow_finnloop.py
@@ -768,6 +768,7 @@ def test_finnloop_end2end_mlo(
         steps=steps,
         synth_clk_period_ns=10.0,
         board="V80",
+        target_fps=1,  # dummy: folding done by custom build steps, not by target_fps
         rtlsim_batch_size=100,
         standalone_thresholds=True,
         mlo=True,
@@ -1016,6 +1017,7 @@ def test_finnloop_end2end_mlo_ddr(
         synth_clk_period_ns=10.0,
         board="AUP-ZU3_8GB",
         shell_flow_type=build_cfg.ShellFlowType.VIVADO_ZYNQ,
+        target_fps=1,  # dummy: folding done by custom build steps, not by target_fps
         rtlsim_batch_size=100,
         standalone_thresholds=True,
         mlo=True,
