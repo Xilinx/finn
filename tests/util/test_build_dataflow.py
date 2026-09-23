8eafa965424c338870127f8ef1c9809ab5b75e8c
auphelia <jakobapk@web.de>
2026-09-23 16:06:26 +0100

    [Transform] First draft of transformation to create portable rtl from FINN IP

--- tests/util/test_build_dataflow.py
+++ tests/util/test_build_dataflow.py
@@ -56,6 +56,13 @@ def test_end2end_build_dataflow_directory():
     assert os.path.isfile(output_dir + "/final_hw_config.json")
     assert os.path.isfile(output_dir + "/template_specialize_layers_config.json")
     assert os.path.isfile(output_dir + "/stitched_ip/ip/component.xml")
+    # Portable RTL export checks
+    assert os.path.isdir(output_dir + "/portable_rtl")
+    assert os.path.isdir(output_dir + "/portable_rtl/rtl")
+    assert os.path.isdir(output_dir + "/portable_rtl/data")
+    assert os.path.isfile(output_dir + "/portable_rtl/filelist.f")
+    assert os.path.isfile(output_dir + "/portable_rtl/sources.tcl")
+    assert os.path.isfile(output_dir + "/portable_rtl/README.md")
     assert os.path.isfile(output_dir + "/driver/driver.py")
     assert os.path.isfile(output_dir + "/report/estimate_layer_cycles.json")
     assert os.path.isfile(output_dir + "/report/estimate_layer_resources.json")
